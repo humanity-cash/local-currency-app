cd37d7482d7dc4b3465d32f59ae923da6dd2f6fd
Esraa Jbara <jbesraa@gmail.com>
2021-05-27 12:25:16 +0300

    remove older folders

--- utils/common.ts
+++
@@ -1,8 +0,0 @@
-
-export const makeId = () => Math.random().toString(36).substring(7);
-
-export const formatValue = (value: number) => {
-	const strFormat = value.toFixed(2);
-	const [firstPart, secondPart] = strFormat.split('.');
-	return firstPart.replace(/\B(?=(\d{3})+(?!\d))/g, "'") + (secondPart ? "," + secondPart : "");
-}
