06b7cbab19176e60e010157c22e7c78842276b7b
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-06-02 10:11:27 +0200

    fix typescript errors

--- src/uielements/reusable/PersonalDetailsForm.tsx
+++ src/uielements/reusable/PersonalDetailsForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native-elements";
 import { BlockInput } from "../BlockInput";
 import { SelectionProps, SelectModal } from "../SelectModal";
@@ -7,132 +7,159 @@ import { colors } from "../../theme/colors";
 import { View } from "react-native";
 import { useUserDetails } from "../../hooks/useUserDetails";
 import { validateDetailsForm } from "../../utils/validation";
-import { PersonalDetailsErrors } from "../../utils/types";
+import { IMap, PersonalDetailsErrors } from "../../utils/types";
 
-const MAIN_COUNTRY = 'swiss';
+const MAIN_COUNTRY = "swiss";
 
-interface PersonalDetailsState {
-	firstname: string
-	lastname: string
-	nationality: string
-	birthDay: string
-	birthMonth: string
-	birthYear: string
+interface PersonalDetailsState extends IMap {
+  firstname: string;
+  lastname: string;
+  nationality: string;
+  birthDay: string;
+  birthMonth: string;
+  birthYear: string;
 }
 
 interface PersonalDetailsProps {
-	isValid: (valid: boolean) => void,
-	showValidation?: boolean
+  isValid: (valid: boolean) => void;
+  showValidation?: boolean;
 }
 
 export const PersonalDetailsForm = (props: PersonalDetailsProps) => {
-	const { personalDetails, updatePersonalDetails } = useUserDetails();
-	const [validationErrors, setValidationErrors] = useState<PersonalDetailsErrors>({});
-	const [state, setState] = useState<PersonalDetailsState>({
-		firstname: '',
-		lastname: '',
-		nationality: '',
-		birthDay: '',
-		birthMonth: '',
-		birthYear: ''
-	});
-	const { showValidation } = props;
+  const { personalDetails, updatePersonalDetails } = useUserDetails();
+  const [
+    validationErrors,
+    setValidationErrors,
+  ] = useState<PersonalDetailsErrors>({});
+  const [state, setState] = useState<PersonalDetailsState>({
+    firstname: "",
+    lastname: "",
+    nationality: "",
+    birthDay: "",
+    birthMonth: "",
+    birthYear: "",
+  });
+  const { showValidation } = props;
 
-	useEffect(() => {
-		const validation = validateDetailsForm(personalDetails);
-		setValidationErrors(validation.errors);
-	}, [personalDetails]);
+  useEffect(() => {
+    const validation = validateDetailsForm(personalDetails);
+    setValidationErrors(validation.errors);
+  }, [personalDetails]);
 
-	useEffect(() => {
-		props.isValid(Object.keys(state).every((key) => state[key] !== ''));
-	}, [state]);
+  useEffect(() => {
+    props.isValid(Object.keys(state).every((key) => state[key] !== ""));
+  }, [state]);
 
-	useEffect(() => {
-		setState({
-			firstname: personalDetails.firstname,
-			lastname: personalDetails.lastname,
-			nationality: personalDetails.nationality,
-			birthDay: personalDetails.dateOfBirth.day,
-			birthMonth: personalDetails.dateOfBirth.month,
-			birthYear: personalDetails.dateOfBirth.year
-		});
-	}, [personalDetails]);
+  useEffect(() => {
+    setState({
+      firstname: personalDetails.firstname,
+      lastname: personalDetails.lastname,
+      nationality: personalDetails.nationality,
+      birthDay: personalDetails.dateOfBirth.day,
+      birthMonth: personalDetails.dateOfBirth.month,
+      birthYear: personalDetails.dateOfBirth.year,
+    });
+  }, [personalDetails]);
 
-	const onValueChange = (name: any, change: any) => {
-		setState({
-			...state,
-			[name]: change
-		} as any)
-		if (name === 'day' || name === 'month' || name === 'year') {
-			updatePersonalDetails({ dateOfBirth: { ...personalDetails.dateOfBirth, [name]: change } });
-			return;
-		}
-		updatePersonalDetails({ [name]: change });
-	}
+  const onValueChange = (name: any, change: any) => {
+    setState({
+      ...state,
+      [name]: change,
+    } as any);
+    if (name === "day" || name === "month" || name === "year") {
+      updatePersonalDetails({
+        dateOfBirth: { ...personalDetails.dateOfBirth, [name]: change },
+      });
+      return;
+    }
+    updatePersonalDetails({ [name]: change });
+  };
 
-	return (
-		<View>
-			<Text h3>first name as in passport</Text>
-			{showValidation && validationErrors.firstname && (<Text h3 style={{ marginTop: 5, color: colors.textError}}>{validationErrors.firstname}</Text>)}
-			<BlockInput
-				name="firstname"
-				placeholder="First name (as in passport)"
-				value={state.firstname}
-				onChange={onValueChange}
-			/>
-			<Text h3>last name as in passport</Text>
-			{showValidation && validationErrors.lastname && (<Text h3 style={{ marginTop: 5, color: colors.textError}}>{validationErrors.lastname}</Text>)}
-			<BlockInput
-				name="lastname"
-				placeholder="Last name (as in passport)"
-				value={state.lastname}
-				onChange={onValueChange}
-			/>
-			<Text h3>nationality</Text>
-			{showValidation && validationErrors.nationality && (<Text h3 style={{ marginTop: 5, color: colors.textError}}>{validationErrors.nationality}</Text>)}
-			<SelectModal
-				name="nationality"
-				value={state.nationality}
-				onChange={onValueChange}
-				modalHeader="Select your nationality"
-				modalMainOption={countries.find((country: SelectionProps) => country.value === MAIN_COUNTRY)}
-				modalList={countries.filter((country: SelectionProps) => country.value !== MAIN_COUNTRY)}
-				modalListLabel="other"
-			/>
-			<Text h3>Date of birth</Text>
-			{showValidation && validationErrors.dateOfBirth && (<Text h3 style={{ marginTop: 5, color: colors.textError}}>{validationErrors.dateOfBirth}</Text>)}
-			<View style={{
-				flexDirection: "row",
-				width: '60%'
-			}}>
-				<BlockInput
-					style={{ flex: 1, textAlign: 'center' }}
-					placeholder="DD"
-					keyboardType="number-pad"
-					name="day"
-					maxLength={2}
-					value={state.birthDay}
-					onChange={onValueChange}
-				/>
-				<BlockInput
-					style={{ marginLeft: 5, flex: 1, textAlign: 'center' }}
-					placeholder="MM"
-					keyboardType="number-pad"
-					name="month"
-					maxLength={2}
-					value={state.birthMonth}
-					onChange={onValueChange}
-				/>
-				<BlockInput
-					style={{ marginLeft: 5, flex: 2, textAlign: 'center' }}
-					placeholder="YYYY"
-					keyboardType="number-pad"
-					name="year"
-					maxLength={4}
-					value={state.birthYear}
-					onChange={onValueChange}
-				/>
-			</View>
-		</View>
-	)
-}
+  return (
+    <View>
+      <Text h3>first name as in passport</Text>
+      {showValidation && validationErrors.firstname && (
+        <Text h3 style={{ marginTop: 5, color: colors.textError }}>
+          {validationErrors.firstname}
+        </Text>
+      )}
+      <BlockInput
+        name="firstname"
+        placeholder="First name (as in passport)"
+        value={state.firstname}
+        onChange={onValueChange}
+      />
+      <Text h3>last name as in passport</Text>
+      {showValidation && validationErrors.lastname && (
+        <Text h3 style={{ marginTop: 5, color: colors.textError }}>
+          {validationErrors.lastname}
+        </Text>
+      )}
+      <BlockInput
+        name="lastname"
+        placeholder="Last name (as in passport)"
+        value={state.lastname}
+        onChange={onValueChange}
+      />
+      <Text h3>nationality</Text>
+      {showValidation && validationErrors.nationality && (
+        <Text h3 style={{ marginTop: 5, color: colors.textError }}>
+          {validationErrors.nationality}
+        </Text>
+      )}
+      <SelectModal
+        name="nationality"
+        value={state.nationality}
+        onChange={onValueChange}
+        modalHeader="Select your nationality"
+        modalMainOption={countries.find(
+          (country: SelectionProps) => country.value === MAIN_COUNTRY
+        )}
+        modalList={countries.filter(
+          (country: SelectionProps) => country.value !== MAIN_COUNTRY
+        )}
+        modalListLabel="other"
+      />
+      <Text h3>Date of birth</Text>
+      {showValidation && validationErrors.dateOfBirth && (
+        <Text h3 style={{ marginTop: 5, color: colors.textError }}>
+          {validationErrors.dateOfBirth}
+        </Text>
+      )}
+      <View
+        style={{
+          flexDirection: "row",
+          width: "60%",
+        }}
+      >
+        <BlockInput
+          style={{ flex: 1, textAlign: "center" }}
+          placeholder="DD"
+          keyboardType="number-pad"
+          name="day"
+          maxLength={2}
+          value={state.birthDay}
+          onChange={onValueChange}
+        />
+        <BlockInput
+          style={{ marginLeft: 5, flex: 1, textAlign: "center" }}
+          placeholder="MM"
+          keyboardType="number-pad"
+          name="month"
+          maxLength={2}
+          value={state.birthMonth}
+          onChange={onValueChange}
+        />
+        <BlockInput
+          style={{ marginLeft: 5, flex: 2, textAlign: "center" }}
+          placeholder="YYYY"
+          keyboardType="number-pad"
+          name="year"
+          maxLength={4}
+          value={state.birthYear}
+          onChange={onValueChange}
+        />
+      </View>
+    </View>
+  );
+};
--- src/views/wallet/WalletAddCash.tsx
+++ src/views/wallet/WalletAddCash.tsx
@@ -1,9 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, ScrollView, View, Keyboard } from 'react-native';
-import { Text } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from '../../uielements/Button'
-import { modalViewBase, baseHeader, wrappingContainerBase } from "../../theme/elements";
+import React, { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  ScrollView,
+  View,
+  Keyboard,
+} from "react-native";
+import { Text } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "../../uielements/Button";
+import {
+  modalViewBase,
+  baseHeader,
+  wrappingContainerBase,
+} from "../../theme/elements";
 import { usePaymentDetails } from "../../hooks/usePaymentDetails";
 import { CancelBtn } from "../../uielements/header/CancelBtn";
 import { ModalHeader } from "../../uielements/header/ModalHeader";
@@ -11,101 +22,110 @@ import { MODAL_SCREEN_OFFSET } from "../../uielements/Modal";
 import { BorderedInput } from "../../uielements/BorderedInput";
 import { useWallet } from "../../hooks/useWallet";
 import { colors } from "../../theme/colors";
+import { IMap } from "../../utils/types";
 
-interface WalletAddCashState {
-	amount: string
+interface WalletAddCashState extends IMap {
+  amount: string;
 }
 
 type WalletAddCashProps = {
-	navigation?: any
-	route?: any
-}
+  navigation?: any;
+  route?: any;
+};
 
 const styles = StyleSheet.create({
-	view: {
-		marginTop: 10
-	},
-	header: {
-		fontFamily: 'IBMPlexSansBold',
-		fontSize: 20,
-	},
-	text: {
-		fontSize: 20,
-		lineHeight: 60,
-		marginRight: 10,
-		fontFamily: 'IBMPlexSansSemiBold',
-	}
+  view: {
+    marginTop: 10,
+  },
+  header: {
+    fontFamily: "IBMPlexSansBold",
+    fontSize: 20,
+  },
+  text: {
+    fontSize: 20,
+    lineHeight: 60,
+    marginRight: 10,
+    fontFamily: "IBMPlexSansSemiBold",
+  },
 });
 
 const WalletAddCashView = (props: WalletAddCashProps) => {
-	const { update } = usePaymentDetails();
-	const { wallet } = useWallet();
-	const [state, setState] = useState<WalletAddCashState>({
-		amount: '0'
-	});
-	const [goNext, setGoNext] = useState(false);
-	const [lowBalance, setLowBalance] = useState(false);
+  const { update } = usePaymentDetails();
+  const { wallet } = useWallet();
+  const [state, setState] = useState<WalletAddCashState>({
+    amount: "0",
+  });
+  const [goNext, setGoNext] = useState(false);
+  const [lowBalance, setLowBalance] = useState(false);
 
-	useEffect(() => {
-		update({ amount: state.amount });
-	}, []);
+  useEffect(() => {
+    update({ amount: state.amount });
+  }, []);
 
-	useEffect(() => {
-		setGoNext(Object.keys(state).every((key) => state[key] !== '' && parseInt(state[key]) > '0'));
-	}, [state]);
+  useEffect(() => {
+    setGoNext(
+      Object.keys(state).every(
+        (key) => state[key] !== "" && parseInt(state[key]) > 0
+      )
+    );
+  }, [state]);
 
-	const onValueChange = (name: any, change: any) => {
-		setState({
-			...state,
-			[name]: change
-		} as any)
-		update({ [name]: change });
-	}
+  const onValueChange = (name: any, change: any) => {
+    setState({
+      ...state,
+      [name]: change,
+    } as any);
+    update({ [name]: change });
+  };
 
-	return (
-		<View style={modalViewBase}>
-			<ModalHeader
-				rightComponent={<CancelBtn onClick={props.route.params.onClose} />}
-			/>
+  return (
+    <View style={modalViewBase}>
+      <ModalHeader
+        rightComponent={<CancelBtn onClick={props.route.params.onClose} />}
+      />
 
-			<ScrollView style={wrappingContainerBase}>
-				<View style={{ paddingBottom: 40 }}>
-					<View style={ baseHeader }>
-						<Text style={styles.header}>Add cash</Text>
-					</View>
-					<View style={styles.view}>
-						{lowBalance && (<Text h3 style={{ color: colors.textWarning }}>Sorry, that exceeds your wallet balance</Text>)}
-						<BorderedInput
-							label="Amount"
-							keyboardType="number-pad"
-							name="amount"
-							prefix="CHF"
-							value={state.amount}
-							onChange={onValueChange}
-						/>
-					</View>
-				</View>
-			</ScrollView>
-			<KeyboardAvoidingView
-				behavior={Platform.OS == "ios" ? "padding" : "height"}
-				keyboardVerticalOffset={MODAL_SCREEN_OFFSET}
-			>
-				<Button
-					type="fluidDark"
-					title="NEXT"
-					disabled={!goNext}
-					onPress={() => {
-						Keyboard.dismiss();
-						props.navigation.navigate('WalletSelectPayment');
-					}}
-				/>
-			</KeyboardAvoidingView>
-		</View>
-	);
-}
+      <ScrollView style={wrappingContainerBase}>
+        <View style={{ paddingBottom: 40 }}>
+          <View style={baseHeader}>
+            <Text style={styles.header}>Add cash</Text>
+          </View>
+          <View style={styles.view}>
+            {lowBalance && (
+              <Text h3 style={{ color: colors.textWarning }}>
+                Sorry, that exceeds your wallet balance
+              </Text>
+            )}
+            <BorderedInput
+              label="Amount"
+              keyboardType="number-pad"
+              name="amount"
+              prefix="CHF"
+              value={state.amount}
+              onChange={onValueChange}
+            />
+          </View>
+        </View>
+      </ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={MODAL_SCREEN_OFFSET}
+      >
+        <Button
+          type="fluidDark"
+          title="NEXT"
+          disabled={!goNext}
+          onPress={() => {
+            Keyboard.dismiss();
+            props.navigation.navigate("WalletSelectPayment");
+          }}
+        />
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
 
 const WalletAddCash = (props: WalletAddCashProps) => {
-	const navigation = useNavigation();
-	return <WalletAddCashView {...props} navigation={navigation} />;
-}
-export default WalletAddCash
+  const navigation = useNavigation();
+  return <WalletAddCashView {...props} navigation={navigation} />;
+};
+export default WalletAddCash;
