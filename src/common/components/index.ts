cb6ee676a3b1c747a16fc7ed5fc0ea1578ab70f8
Esraa Jbara <jbesraa@gmail.com>
2021-06-25 11:43:34 +0300

    add phone number input

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button';
 import Input from './Input';
 import PinCode from './PinCode';
+import PhoneNumberInput from './PhonenNumberInput';
 
-export { Button, Input, PinCode };
+export { Button, Input, PinCode, PhoneNumberInput };
 
