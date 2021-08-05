37fde3c1680601e51a44e74ea97de997fb58a105
JeyDev310 <mtroz310@gmail.com>
2021-08-06 02:40:36 +0800

    feat(ui): added settings page

---
+++ src/screens/settings/SettingsPersonalProfile.tsx
@@ -0,0 +1,146 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, View, TouchableOpacity, Platform, Image, KeyboardAvoidingView} from "react-native";
+import { Text } from "react-native-elements";
+import * as ImagePicker from 'expo-image-picker';
+import { useUserDetails } from "src/hooks";
+import { Header, BackBtn, BlockInput, Button } from "src/shared/uielements";
+import { colors } from "src/theme/colors";
+import { viewBase, underlineHeader } from "src/theme/elements";
+
+interface PersonalProfileState {
+	avatar: string;
+	username: string;
+}
+
+const styles = StyleSheet.create({
+	container: { 
+		flex: 1, 
+		padding: 10 ,
+	},
+	headerText: {
+		fontSize: 32,
+		fontWeight: '400',
+		lineHeight: 40
+	},
+	contentView: {
+		marginTop: 50,
+		paddingBottom: 40
+	},
+	imageView: {
+		flex: 1,
+		alignSelf: 'center'
+	},
+	placeholder: {
+		width: 80, 
+		height: 80, 
+		backgroundColor: colors.darkGreen,
+		borderRadius: 40
+	},
+	imageDesc1: {
+		fontSize: 16,
+		color: colors.bodyText, 
+		textAlign: "center", 
+		marginTop: 10,
+	},
+	imageDesc2: {
+		fontSize: 10,
+		textAlign: "center", 
+		marginBottom: 10
+	},
+	bottomView: {
+		paddingHorizontal: 20,
+		paddingBottom: 60,
+	}
+});
+
+export const SettingsPersonalDetails = () => {
+	const navigation = useNavigation();
+	const { personalDetails, updatePersonalDetails } = useUserDetails();
+	const [canSave, setCanSave] = useState<boolean>(false);
+	const [state, setState] = useState<PersonalProfileState>({
+		avatar: "",
+		username: ""
+	});
+
+	useEffect(() => {
+		setCanSave(state.username !== "");
+	}, [state]);
+
+	useEffect(() => {
+		(async () => {
+		  if (Platform.OS !== 'web') {
+			const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+			if (status !== 'granted') {
+			  alert('Sorry, we need camera roll permissions to make this work!');
+			}
+		  }
+		})();
+	}, []);
+
+	const onValueChange = (name: any, change: any) => {
+		setState({
+		  ...state,
+		  [name]: change,
+		} as any);
+		updatePersonalDetails({ [name]: change });
+	};
+
+	const pickImage = async () => {
+		const result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+	
+		if (!result.cancelled) {
+		  	onValueChange('avatar', result.uri);
+		}
+	};
+
+	return (
+		<View style={viewBase}>
+			<Header
+				leftComponent={<BackBtn onClick={() => navigation.goBack()} />}
+			/>
+			<ScrollView style={styles.container}>
+				<View style={ underlineHeader }>
+					<Text style={styles.headerText}>My profile</Text>
+				</View>
+				<Text>This information is shared publicly.</Text>
+				<View style={styles.contentView}>
+					<View style={styles.imageView}>
+						<TouchableOpacity onPress={pickImage}>
+						{state.avatar === '' && <View style={styles.placeholder} />}
+						{state.avatar !== '' && <Image source={{ uri: state.avatar }} style={styles.placeholder} />}
+						</TouchableOpacity>
+					</View>
+					<Text style={styles.imageDesc1}>Change profile picture</Text>
+					<Text style={styles.imageDesc2}>(MAX 200MB / JPG, JPEG, PNG)</Text>
+					<Text h3>USER NAME</Text>
+					<BlockInput
+						name="username"
+						placeholder="@username"
+						value={state.username}
+						onChange={onValueChange}
+					/>
+				</View>
+			</ScrollView>
+			<KeyboardAvoidingView
+				behavior={Platform.OS == "ios" ? "padding" : "height"}
+			>
+				<View style={styles.bottomView}>
+					<Button
+						type="darkGreen"
+						title="Save changes"
+						disabled={!canSave}
+						onPress={()=>setCanSave(true)}
+					/>
+				</View>
+			</KeyboardAvoidingView>
+		</View>
+	);
+}
+
+export default SettingsPersonalDetails;
--- src/screens/settings/SettingsTermsAndConditions.tsx
+++ src/screens/settings/SettingsTermsAndConditions.tsx
@@ -1,210 +1,58 @@
-import { AntDesign } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import React, { useMemo, useState } from "react";
-import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
-import { Text } from "react-native-elements";
-import { WebView } from "react-native-webview";
-import { useModalStatusBar, useUserDetails } from "src/hooks";
-import { Button, CancelBtn, Header, Modal, ModalHeader, SettingsListItem } from "src/shared/uielements";
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Text } from 'react-native-elements';
+import { Header, BackBtn, AccordionCard } from "src/shared/uielements";
+import { underlineHeader, viewBase } from "src/theme/elements";
 import { colors } from "src/theme/colors";
-import { modalViewBase, viewDashboardBase } from "src/theme/elements";
-import { IMap } from "src/utils/types";
+import { AccordionEntry } from "src/utils/types";
 
 const styles = StyleSheet.create({
-  headerText: {
-    color: colors.white,
-    fontFamily: "IBMPlexSansSemiBold",
-    fontSize: 16,
-  },
-  container: {
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-    backgroundColor: colors.text,
-    zIndex: 100,
-    elevation: 10,
-  },
-  headerView: {
-    flexDirection: "row",
-    marginTop: 30,
-    marginBottom: 10,
-  },
-  signOutView: {
-    fontSize: 20,
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    marginBottom: 10,
-    color: colors.white,
-  },
-  signOutButton: {
-    fontSize: 20,
-    fontFamily: "IBMPlexSansSemiBold",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    marginBottom: 10,
-    color: colors.white,
-  },
-  view: {
-    marginTop: 5,
-    backgroundColor: colors.white,
-    padding: 0,
-    flexDirection: "row",
-    paddingHorizontal: 10,
-  },
-  header: {
-    fontFamily: "IBMPlexSansBold",
-    fontSize: 20,
-  },
-  text: {
-    fontSize: 20,
-    lineHeight: 60,
-    paddingLeft: 10,
-    flex: 1,
-    fontFamily: "IBMPlexSansSemiBold",
-  },
-  arrow: {
-    marginVertical: 15,
-  },
-  modalWrap: {
-    paddingHorizontal: 10,
-    height: "100%",
-    flex: 1,
-  },
-  modalHeader: {
-    fontFamily: "IBMPlexSansSemiBold",
-    fontSize: 20,
-    marginBottom: 10,
-  },
+	headerText: {
+		fontSize: 32,
+		fontWeight: '400',
+		lineHeight: 40
+	},
+	container: {
+		flex: 1, 
+		padding: 10
+	},
+	faqView: {
+		paddingBottom: 30, 
+		paddingTop: 10
+	}
 });
 
-const views = {
-  terms: {
-    name: "terms",
-    header: "Terms and Conditions",
-  },
-  privacy: {
-    name: "privacy",
-    header: "Privacy Policy",
-  },
-  banking: {
-    name: "banking",
-    header: "Liendhardt banking account Terms and Conditions",
-  },
-} as IMap;
+const termsAndConditions: AccordionEntry = {
+  title: "Terms & Conditions",
+  content: "ARTICLE 1: GENERAL"
+}
+
+const privacyPolicy: AccordionEntry = {
+  title: "Privacy Policy",
+  content: "Privacy Policy"
+}
 
 export const SettingsTermsAndConditions = () => {
-  const navigation = useNavigation();
-  const { personalDetails } = useUserDetails();
-  const [visible, setVisible] = useState(false);
-  const [viewType, setViewType] = useState({ name: "", header: "" });
-  const { setUseHeader } = useModalStatusBar();
 
-  const documents: { [index: string]: any } = useMemo(
-    () => ({
-      terms: (
-        <WebView
-          style={{ backgroundColor: "transparent" }}
-          source={require("../../../assets/document.pdf")}
-        />
-      ),
-      privacy: (
-        <WebView
-          style={{ backgroundColor: "transparent" }}
-          source={require("../../../assets/document.pdf")}
-        />
-      ),
-      banking: (
-        <WebView
-          style={{ backgroundColor: "transparent" }}
-          source={require("../../../assets/document.pdf")}
-        />
-      ),
-    }),
-    []
-  );
+	const navigation = useNavigation();
 
-  const onModalPress = () => {
-    setUseHeader(false);
-    setVisible(false);
-    setViewType({ name: "", header: "" });
-  };
-
-  const onShow = (type: string) => {
-    setUseHeader(true);
-    setVisible(true);
-    setViewType(views[type]);
-  };
-
-  return (
-    <View style={viewDashboardBase}>
-      <Header
-        placement="left"
-        style={{ backgroundColor: colors.text }}
-        barStyle="light-content"
-        centerComponent={<Text style={styles.headerText}>Dashboard</Text>}
-        leftComponent={
-          <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
-            <View>
-              <AntDesign
-                style={{ paddingTop: 2 }}
-                name="arrowleft"
-                size={25}
-                color={colors.white}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-        }
-      />
-      <View style={{ flex: 1, padding: 10 }}>
-        <View style={styles.headerView}>
-          <Text h1 style={{ color: colors.white, marginBottom: 0 }}>
-            Terms and conditions
-          </Text>
-        </View>
-        <View>
-          <SettingsListItem
-            onPress={() => onShow("terms")}
-            name="Terms and Conditions"
-          />
-          <SettingsListItem
-            onPress={() => onShow("privacy")}
-            name="Privacy Policy"
-          />
-          <SettingsListItem
-            onPress={() => onShow("banking")}
-            name="Liendhardt banking account..."
-          />
-        </View>
-        {visible && (
-          <Modal visible={visible}>
-            <View style={{ ...modalViewBase, height: "100%" }}>
-              <ModalHeader
-                rightComponent={
-                  <CancelBtn
-                    onClick={() => {
-                      setUseHeader(false);
-                      setVisible(false);
-                    }}
-                  />
-                }
-              />
-              <View style={styles.modalWrap}>
-                <Text style={styles.modalHeader}>{viewType.header}</Text>
-                {documents[viewType.name]}
-              </View>
-              <Button
-                type="fluidDark"
-                title={`SEND TO ${personalDetails.email}`}
-                onPress={onModalPress}
-              />
-            </View>
-          </Modal>
-        )}
-      </View>
-    </View>
-  );
-};
+	return (
+		<View style={viewBase}>
+			<Header
+				leftComponent={<BackBtn onClick={() => navigation.goBack()} />}
+			/>
+			<ScrollView style={styles.container}>
+				<View style={ underlineHeader }>
+					<Text style={styles.headerText}>Legal</Text>
+				</View>
+				<View style={styles.faqView}>
+					<AccordionCard title={termsAndConditions.title} content={termsAndConditions.content} />
+          <AccordionCard title={privacyPolicy.title} content={privacyPolicy.content} />
+				</View>
+			</ScrollView>
+		</View>
+	);
+}
 
 export default SettingsTermsAndConditions;
