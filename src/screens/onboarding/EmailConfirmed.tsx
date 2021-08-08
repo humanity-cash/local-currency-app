0bd55f452a90b16317600d6eee4577d33399b784
JeyDev310 <mtroz310@gmail.com>
2021-08-08 22:57:32 +0800

    fix(ui): style corrections with design

--- src/screens/onboarding/EmailConfirmed.tsx
+++ src/screens/onboarding/EmailConfirmed.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Image, Text } from 'react-native-elements';
 import { useUserDetails } from "src/hooks";
-import { Button, Header } from "src/shared/uielements";
+import { Button, Header, BackBtn } from "src/shared/uielements";
 import { baseHeader, viewBase, wrappingContainerBase } from "src/theme/elements";
+import { colors } from "src/theme/colors";
 
 type EmailConfirmedProps = {
 	navigation?: any
@@ -12,15 +13,20 @@ type EmailConfirmedProps = {
 }
 
 const styles = StyleSheet.create({
-	codeView: {
+	headerText: {
+		fontSize: 32,
+		color: colors.darkGreen,
+		lineHeight: 35
+	},
+	bodyText: {
+		color: colors.bodyText
 	},
 	bottomNavigation: {
 		justifyContent: "center"
 	},
 	bottomView: {
-		height: 60,
-		justifyContent: "center",
-		alignItems: 'center'
+		paddingHorizontal: 20,
+        paddingBottom: 50
 	},
 	image: {
 		alignSelf: "center",
@@ -37,28 +43,24 @@ const styles = StyleSheet.create({
 const EmailConfirmedView = (props: EmailConfirmedProps) => {
 	const { personalDetails: { email }} = useUserDetails();
 	return (
-		<View style={{...viewBase}}>
-			<Header />
+		<View style={viewBase}>
+			<Header
+                leftComponent={<BackBtn onClick={() => props.navigation.goBack()} />}
+            />
 
 			<View style={wrappingContainerBase}>
 				<View style={ baseHeader }>
-					<Text h1>Email address confirmed!</Text>
+					<Text style={styles.headerText}>Mail address confirmed!</Text>
 				</View>
-				<View style={styles.codeView}>
-					<Text>Your email address {email} is confirmed. We will use this address to share important documents with you.</Text>
-				</View>
-				<View style={styles.imageView}>
-					<Image
-						source={require('../../../assets/images/baloons.png')}
-						containerStyle={styles.image}
-					/>
-				</View>
+				<Text style={styles.bodyText}>Your email address {email} is confirmed.</Text>
 			</View>
-			<Button
-				type="fluidDark"
-				title="NEXT"
-				onPress={() => props.navigation.navigate('Terms')}
-			/>
+			<View style={styles.bottomView}>
+				<Button
+					type="darkGreen"
+					title="NEXT"
+					onPress={() => props.navigation.navigate("Password")}
+				/>
+			</View>
 		</View>
 	);
 }
