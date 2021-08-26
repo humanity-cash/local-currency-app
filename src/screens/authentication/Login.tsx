f7bbf610d2305766a7370119d3b7f6716e6d39e3
Esraa Jbara <jbesraa@gmail.com>
2021-08-26 16:14:51 +0300

    refactor login screen

--- src/screens/authentication/Login.tsx
+++ src/screens/authentication/Login.tsx
@@ -1,94 +1,62 @@
-import { useIsFocused, useNavigation } from '@react-navigation/native';
-import * as LocalAuthentication from "expo-local-authentication";
-import React, { useEffect, useState } from 'react';
-import { ScrollView, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useContext } from 'react';
+import {
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	View,
+} from 'react-native';
 import { Text } from 'react-native-elements';
-import { useUserDetails } from "src/hooks";
-import { Header, BackBtn, BlockInput, Button } from "src/shared/uielements";
-import { baseHeader, viewBase, wrappingContainerBase } from "src/theme/elements";
-import ForgotPassword from "./ForgotPassword";
-import { colors } from "src/theme/colors";
-import { IMap } from "src/utils/types";
-
-type LoginProps = {
-	navigation?: any,
-	route: any
-}
-
-interface LoginForm extends IMap {
-	password: string
-	email: string
-}
+import { Header, BackBtn, BlockInput, Button } from 'src/shared/uielements';
+import {
+	baseHeader,
+	viewBase,
+	wrappingContainerBase,
+} from 'src/theme/elements';
+import { colors } from 'src/theme/colors';
+import { AuthContext } from 'src/auth';
+import { BUTTON_TYPES, SCREENS } from 'src/constants';
+import { SignInInput } from 'src/auth/types';
 
 const styles = StyleSheet.create({
 	headerText: {
 		fontSize: 32,
 		color: colors.darkGreen,
-		lineHeight: 35
+		lineHeight: 35,
 	},
 	bodyText: {
-		color: colors.bodyText
+		color: colors.bodyText,
 	},
 	form: {
-		marginVertical: 30
+		marginVertical: 30,
 	},
 	label: {
 		fontSize: 12,
 		lineHeight: 14,
-		color: colors.bodyText
+		color: colors.bodyText,
 	},
 	bottomView: {
 		paddingHorizontal: 20,
-    	paddingBottom: 50
+		paddingBottom: 50,
 	},
 });
 
-//eslint-disable-next-line
-const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+const Login = (): JSX.Element => {
+	const navigation = useNavigation();
+	const { signIn, signInDetails, setSignInDetails } = useContext(AuthContext);
 
-//eslint-disable-next-line
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-
-const LoginView = (props: LoginProps) => {
-	const { authorization: { touchID } } = useUserDetails();
-	const isFocused = useIsFocused();
-	const [autoFocus, setAutoFocus] = useState<boolean>(true);
-	const [goNext, setGoNext] = useState<boolean>(true);
-	const [state, setState] = useState<LoginForm>({
-		email: "",
-		password: ""
-	})
-	
-	useEffect(() => {
-		setGoNext(Object.keys(state).every((key) => state[key] !== "") && strongRegex.test(state.password) && emailRegex.test(state.email));
-	},[state]);
-
-	useEffect(() => {
-		async function askFingerprint() {
-			if (isFocused && touchID) {
-				const data = await LocalAuthentication.authenticateAsync({
-					disableDeviceFallback: true,
-					cancelLabel: 'Close'
-				});
-				setAutoFocus(false);
-			}
-		}
-		askFingerprint();
-
-		setAutoFocus(isFocused);
-	}, [isFocused, touchID]);
-
-	const onValueChange = (name: any, change: any) => {
-		setState({
-			...state,
-			[name]: change
-		});
+	const onValueChange = (name: 'email' | 'password', change: string) => {
+		setSignInDetails((pv: SignInInput) => ({
+			...pv,
+			[name]: change,
+		}));
 	};
 
 	return (
 		<View style={viewBase}>
 			<Header
-				leftComponent={<BackBtn onClick={() => props.navigation.goBack()} />}
+				leftComponent={<BackBtn onClick={() => navigation.goBack()} />}
 			/>
 
 			<ScrollView style={wrappingContainerBase}>
@@ -99,16 +67,16 @@ const LoginView = (props: LoginProps) => {
 				<View style={styles.form}>
 					<Text style={styles.label}>Email address or user name</Text>
 					<BlockInput
-						name="email"
-						placeholder="Email"
-						value={state.email}
+						name='email'
+						placeholder='Email'
+						value={signInDetails.email}
 						onChange={onValueChange}
 					/>
 					<Text style={styles.label}>Password</Text>
 					<BlockInput
-						name="password"
-						placeholder="Password"
-						value={state.password}
+						name='password'
+						placeholder='Password'
+						value={signInDetails.password}
 						secureTextEntry={true}
 						onChange={onValueChange}
 					/>
@@ -116,28 +84,44 @@ const LoginView = (props: LoginProps) => {
 			</ScrollView>
 
 			<KeyboardAvoidingView
-				behavior={Platform.OS == "ios" ? "padding" : "height"}
-			>
+				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
 				<View style={styles.bottomView}>
 					<Button
-						type="transparent"
-						title="Forgot Passowrd?"
-						onPress={() => props.navigation.navigate("ForgotPassword")}
+						type={BUTTON_TYPES.TRANSPARENT}
+						title='Forgot Passowrd?'
+						onPress={() =>
+							navigation.navigate(SCREENS.FORGOT_PASSWORD)
+						}
 					/>
 					<Button
-						type="darkGreen"
-						title="Log in"
-						disabled={!goNext}
-						onPress={() => props.navigation.navigate("SelectAccountType")}
+						type={BUTTON_TYPES.DARK_GREEN}
+						title='Log in'
+						disabled={false}
+						onPress={signIn}
 					/>
 				</View>
 			</KeyboardAvoidingView>
 		</View>
 	);
-}
+};
 
-const Login = (props:LoginProps) => {
-	const navigation = useNavigation();
-	return <LoginView {...props} navigation={navigation} />;
-}
-export default Login
+export default Login;
+
+// useEffect(() => {
+// 	async function askFingerprint() {
+// 		if (isFocused && touchID) {
+// 			const data = await LocalAuthentication.authenticateAsync({
+// 				disableDeviceFallback: true,
+// 				cancelLabel: 'Close'
+// 			});
+// 			setAutoFocus(false);
+// 		}
+// 	}
+// 	askFingerprint();
+
+// 	setAutoFocus(isFocused);
+// }, [isFocused, touchID]);
+
+// const isFocused = useIsFocused();
+// const [autoFocus, setAutoFocus] = useState<boolean>(true);
+
