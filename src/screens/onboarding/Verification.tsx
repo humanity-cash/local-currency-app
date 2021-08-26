3829b5ca9d3d26b40fe16dc8084cff3c9c9874ce
Esraa Jbara <jbesraa@gmail.com>
2021-08-26 17:04:47 +0300

    refactor verification screen

--- src/screens/onboarding/Verification.tsx
+++ src/screens/onboarding/Verification.tsx
@@ -1,107 +1,145 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { Keyboard, ScrollView, KeyboardAvoidingView, Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import {
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    TouchableOpacity,
+    View
+} from 'react-native';
 import { Text } from 'react-native-elements';
-import { useUserDetails } from "src/hooks";
-import { BackBtn, ConfirmationCode, Header, Button } from "src/shared/uielements";
-import { baseHeader, viewBase, wrappingContainerBase } from "src/theme/elements";
-import { colors } from "src/theme/colors";
-
-type VerificationProps = {
-    navigation?: any
-    route?: any
-}
+import { AuthContext } from 'src/auth';
+import { BUTTON_TYPES, SCREENS } from 'src/constants';
+import { useUserDetails } from 'src/hooks';
+import {
+    BackBtn,
+    Button,
+    ConfirmationCode,
+    Header
+} from 'src/shared/uielements';
+import { colors } from 'src/theme/colors';
+import {
+    baseHeader,
+    viewBase,
+    wrappingContainerBase
+} from 'src/theme/elements';
 
 const styles = StyleSheet.create({
-    container: {
-        paddingBottom: 40
-    },
-    headerText: {
+	container: {
+		paddingBottom: 40,
+	},
+	headerText: {
 		fontSize: 32,
-        color: colors.darkGreen,
-        lineHeight: 32
+		color: colors.darkGreen,
+		lineHeight: 32,
 	},
-    bodyText: {
-		color: colors.bodyText
+	bodyText: {
+		color: colors.bodyText,
 	},
-    codeView: {
-        flex: 1,
-        marginTop: 25
-    },
-    bottomNavigation: {
-        alignSelf: "center",
-        color: colors.darkGreen,
-        fontWeight: 'bold',
-        paddingVertical: 30
-    },
-    bottomView: {
+	codeView: {
+		flex: 1,
+		marginTop: 25,
+	},
+	bottomNavigation: {
+		alignSelf: 'center',
+		color: colors.darkGreen,
+		fontWeight: 'bold',
+		paddingVertical: 30,
+	},
+	bottomView: {
 		paddingHorizontal: 20,
-        paddingBottom: 50
+		paddingBottom: 50,
 	},
 });
 
-const VALID_CODE = '123456';
+const Verification = (): JSX.Element => {
+	const navigation = useNavigation();
+	const { emailVerification, resendEmailVerificationCode } =
+		useContext(AuthContext);
+	const [noCodeReceived, setNoCodeReceived] = useState<boolean>(false);
+	const [goNext, setGoNext] = useState<boolean>(false);
+	const { personalDetails } = useUserDetails();
 
-const VerificationView = (props: VerificationProps) => {
-    const [noCodeReceived, setNoCodeReceived] = useState<boolean>(false);
-    const [goNext, setGoNext] = useState<boolean>(false);
-    const { personalDetails } = useUserDetails();
+	const onComplete = async (text: string) => {
+		const response = await emailVerification(text);
+		if (response.success) {
+			setGoNext(true);
+		}
+	};
 
-    const onComplete = (text: string) => {
-        setGoNext(true);
-    }
+	return (
+		<View style={viewBase}>
+			<Header
+				leftComponent={
+					<BackBtn onClick={() => navigation.goBack()} />
+				}
+			/>
 
-    return (
-        <View style={viewBase}>
-            <Header
-                leftComponent={<BackBtn onClick={() => props.navigation.goBack()} />}
-            />
+			<ScrollView style={wrappingContainerBase}>
+				<View style={styles.container}>
+					<View style={baseHeader}>
+						{!noCodeReceived && (
+							<Text style={styles.headerText}>
+								Verify your mail address
+							</Text>
+						)}
+						{noCodeReceived && (
+							<Text style={styles.headerText}>
+								Enter verification code
+							</Text>
+						)}
+					</View>
+					{!noCodeReceived && (
+						<Text style={styles.bodyText}>
+							We have sent an email with a verification code to{' '}
+							{personalDetails.email}
+						</Text>
+					)}
+					{noCodeReceived && (
+						<Text style={styles.bodyText}>
+							We have sent another message with a new verification
+							code to {personalDetails.email}
+						</Text>
+					)}
+					<View style={styles.codeView}>
+						<ConfirmationCode onComplete={onComplete} />
+					</View>
+				</View>
+			</ScrollView>
 
-            <ScrollView style={wrappingContainerBase}>
-                <View style={styles.container}>
-                    <View style={baseHeader}>
-                        {!noCodeReceived && <Text style={styles.headerText}>Verify your mail address</Text>}
-                        {noCodeReceived && <Text style={styles.headerText}>Enter verification code</Text>}
-                    </View>
-                    {!noCodeReceived && (<Text style={styles.bodyText}>We have sent an email with a verification code to {personalDetails.email}</Text>)}
-                    {noCodeReceived && (<Text style={styles.bodyText}>We have sent another message with a new verification code to {personalDetails.email}</Text>)}
-                    <View style={styles.codeView}>
-                        <ConfirmationCode onComplete={onComplete} />
-                    </View>
-                </View>
-            </ScrollView>
+			<KeyboardAvoidingView
+				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+				<View style={styles.bottomView}>
+					{!noCodeReceived && (
+						<TouchableOpacity onPress={resendEmailVerificationCode}>
+							<Text style={styles.bottomNavigation}>
+								Send code again
+							</Text>
+						</TouchableOpacity>
+					)}
+					{noCodeReceived && (
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate(SCREENS.VERIFICATION_HELP)
+							}>
+							<Text style={styles.bottomNavigation}>
+								Need help?
+							</Text>
+						</TouchableOpacity>
+					)}
+					<Button
+						type={BUTTON_TYPES.DARK_GREEN}
+						title='NEXT'
+						disabled={!goNext}
+						onPress={() =>
+							navigation.navigate(SCREENS.EMAIL_CONFIRMED)
+						}
+					/>
+				</View>
+			</KeyboardAvoidingView>
+		</View>
+	);
+};
 
-            <KeyboardAvoidingView
-                behavior={Platform.OS == "ios" ? "padding" : "height"}
-            >
-                <View style={styles.bottomView}>
-                    {!noCodeReceived && (
-                        <TouchableOpacity onPress={() => {
-                            setNoCodeReceived(true);
-                            Keyboard.dismiss();
-                        }}>
-                            <Text style={styles.bottomNavigation}>Send code again</Text>
-                        </TouchableOpacity>
-                    )}
-                    {noCodeReceived && (
-                        <TouchableOpacity onPress={() => props.navigation.navigate('VerificationHelp')}>
-                            <Text style={styles.bottomNavigation}>Need help?</Text>
-                        </TouchableOpacity>
-                    )}
-                    <Button
-                        type="darkGreen"
-                        title="NEXT"
-                        disabled={!goNext}
-                        onPress={() => props.navigation.navigate("EmailConfirmed")}
-                    />
-                </View>
-            </KeyboardAvoidingView>
-        </View>
-    );
-}
-
-const Verification = (props:VerificationProps) => {
-    const navigation = useNavigation();
-    return <VerificationView {...props} navigation={navigation} />;
-}
-export default Verification
+export default Verification;
