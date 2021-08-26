8ec6c8c3f84e37e63dcb10b2571d523751015eaf
Esraa Jbara <jbesraa@gmail.com>
2021-08-26 17:22:46 +0300

    disable lint for regex

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -102,10 +102,11 @@ export const validateBusinessDetailsForm = (businessDetails: BusinessDetails) =>
 	return { errors, valid: Object.keys(errors).length === 0 };
 }
 
-//@ts-ignore
+// eslint-disable-next-line
 const passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-//@ts-ignore
+// eslint-disable-next-line
 const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const isPasswordValid = (p: string): boolean => passwordRegex.test(p)
 
 export const isEmailValid = (e: string): boolean => emailValidation.test(e)
