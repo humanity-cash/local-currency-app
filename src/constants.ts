0bf38bae99b9389eb5db4b3b41ec01895f18cc2f
Esraa Jbara <jbesraa@gmail.com>
2021-08-27 09:03:52 +0300

    merge conflicts fixes

--- src/constants.ts
+++ src/constants.ts
@@ -3,25 +3,3 @@ export const BUTTON_TYPES = {
 	DARK_RED: 'darkRed',
 	TRANSPARENT: 'transparent'
 }
-
-export const SCREENS = {
-	LOGIN: "Login",
-	FORGOT_PASSWORD: "ForgotPassword",
-	CREATE_ACCOUNT: "CreateAccount",
-	QR_CODE_SCAN: "QRCodeScan",
-	TOP_UP: "TopUp",
-	MERCHANT_TABS: "MerchantTabs",
-	SCAN_TO_PAY: "ScanToPay",
-	RECIEVE_PAYMENT: "ReceivePayment",
-	LOAD_UP: "LoadUp",
-	CASH_OUT: "CashOut",
-	MY_TRANSACTIONS: "MyTransactions",
-	WHERE_TO_SPEND: "WhereToSpend",
-	SIGN_UP_YOUR_BUSINESS: "SignUpYourBusiness",
-	SETTINGS: "Settings",
-	HELP_AND_CONTACT: "HelpAndContact",
-	PASSWORD: 'Password',
-	VERIFICATION: 'Verification',
-	VERIFICATION_HELP: 'VerificationHelp',
-	EMAIL_CONFIRMED: 'EmailConfirmed'
-}
--- src/screens/settings/SettingsPersonalProfile.tsx
+++ src/screens/settings/SettingsPersonalProfile.tsx
@@ -1,29 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
-<<<<<<< HEAD
-import React, { useState, useEffect, ReactElement } from 'react';
-import { ScrollView, StyleSheet, View, TouchableOpacity, Platform, Image, KeyboardAvoidingView} from "react-native";
-import { Text } from "react-native-elements";
-import * as ImagePicker from 'expo-image-picker';
-import { useUserDetails } from "src/hooks";
-import { Header, BackBtn, BlockInput, Button } from "src/shared/uielements";
-import { colors } from "src/theme/colors";
-import { viewBase, underlineHeader } from "src/theme/elements";
-import Translation from 'src/translation/en.json';
-=======
 import * as ImagePicker from 'expo-image-picker';
 import React, { useContext, useEffect, useState } from 'react';
-import {
-	Image,
-	KeyboardAvoidingView, Platform, ScrollView,
-	StyleSheet, TouchableOpacity, View
-} from 'react-native';
-import { Text } from 'react-native-elements';
+import { Image, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Text } from "react-native-elements";
 import { AuthContext } from 'src/auth';
 import { BUTTON_TYPES } from 'src/constants';
-import { BackBtn, BlockInput, Button, Header } from 'src/shared/uielements';
-import { colors } from 'src/theme/colors';
-import { underlineHeader, viewBase } from 'src/theme/elements';
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
+import { BackBtn, BlockInput, Button, Header } from "src/shared/uielements";
+import { colors } from "src/theme/colors";
+import { underlineHeader, viewBase } from "src/theme/elements";
+import Translation from 'src/translation/en.json';
 
 interface PersonalProfileState {
 	avatar: string;
@@ -71,15 +56,9 @@ const styles = StyleSheet.create({
 	},
 });
 
-<<<<<<< HEAD
-export const SettingsPersonalDetails = (): ReactElement => {
-	const navigation = useNavigation();
-	const {personalDetails, updatePersonalDetails} = useUserDetails();
-=======
 export const SettingsPersonalDetails = (): JSX.Element => {
 	const { getAttributes, updateAttributes } = useContext(AuthContext);
 	const navigation = useNavigation();
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
 	const [canSave, setCanSave] = useState<boolean>(false);
 	const [state, setState] = useState<PersonalProfileState>({
 		avatar: '',
@@ -103,45 +82,20 @@ export const SettingsPersonalDetails = (): JSX.Element => {
 	}, [state]);
 
 	useEffect(() => {
-		setState({
-			avatar: personalDetails.avatar,
-			username: personalDetails.username
-		});
-	}, [personalDetails]);
-
-	useEffect(() => {
 		(async () => {
-<<<<<<< HEAD
 		  if (Platform.OS !== 'web') {
 			const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 			if (status !== 'granted') {
 			  alert(Translation.OTHER.NO_CAMERA_PERMISSION);
-=======
-			if (Platform.OS !== 'web') {
-				const { status } =
-					await ImagePicker.requestMediaLibraryPermissionsAsync();
-				if (status !== 'granted') {
-					alert(
-						'Sorry, we need camera roll permissions to make this work!'
-					);
-				}
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
 			}
-		})();
+		}})();
 	}, []);
 
 	const onValueChange = (name: string, change: string) => {
 		setState({
-<<<<<<< HEAD
-		  ...state,
-		  [name]: change,
-		} as PersonalProfileState);
-		updatePersonalDetails({ [name]: change });
-=======
 			...state,
 			[name]: change,
 		});
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
 	};
 
 	const pickImage = async () => {
@@ -163,13 +117,8 @@ export const SettingsPersonalDetails = (): JSX.Element => {
 				leftComponent={<BackBtn onClick={() => navigation.goBack()} />}
 			/>
 			<ScrollView style={styles.container}>
-<<<<<<< HEAD
 				<View style={ underlineHeader }>
 					<Text style={styles.headerText}>{Translation.PROFILE.MY_PROFILE}</Text>
-=======
-				<View style={underlineHeader}>
-					<Text style={styles.headerText}>My profile</Text>
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
 				</View>
 				<Text>{Translation.COMMUNITY_CHEST.INFORMATION_SHARE}</Text>
 				<View style={styles.contentView}>
@@ -186,19 +135,9 @@ export const SettingsPersonalDetails = (): JSX.Element => {
 							)}
 						</TouchableOpacity>
 					</View>
-<<<<<<< HEAD
 					<Text style={styles.imageDesc1}>{Translation.PROFILE.CHANGE_PICTURE}</Text>
 					<Text style={styles.imageDesc2}>{Translation.LABEL.MAX_BERKSHARES}</Text>
 					<Text h3>{Translation.LABEL.USERNAME}</Text>
-=======
-					<Text style={styles.imageDesc1}>
-						Change profile picture
-					</Text>
-					<Text style={styles.imageDesc2}>
-						(MAX 200MB / JPG, JPEG, PNG)
-					</Text>
-					<Text h3>USER NAME</Text>
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
 					<BlockInput
 						name='username'
 						placeholder='@username'
@@ -211,13 +150,8 @@ export const SettingsPersonalDetails = (): JSX.Element => {
 				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
 				<View style={styles.bottomView}>
 					<Button
-<<<<<<< HEAD
-						type="darkGreen"
-						title={Translation.BUTTON.SAVE_CHANGE}
-=======
 						type={BUTTON_TYPES.DARK_GREEN}
-						title='Save changes'
->>>>>>> 9db8db32a9a5185c4f113eced679d15f594dcb89
+						title={Translation.BUTTON.SAVE_CHANGE}
 						disabled={!canSave}
 						onPress={async () => {
 							const response = await updateAttributes({
