e1169fc7ad82354b72517a1981aef17d1abe267c
JeyDev310 <mtroz310@gmail.com>
2021-09-24 18:16:17 +0800

    refactor: change dropdown of mytransaction filter

--- src/screens/transactions/MyTransactionsFilter.tsx
+++ src/screens/transactions/MyTransactionsFilter.tsx
@@ -1,19 +1,14 @@
+import { AntDesign, Entypo } from '@expo/vector-icons';
 import React, {useState} from 'react';
 import { StyleSheet, View, TouchableOpacity, Platform } from 'react-native';
 import { Text } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Dropdown } from 'react-native-material-dropdown';
 import moment from 'moment';
 import { colors } from "src/theme/colors";
 import Translation from 'src/translation/en.json';
+import SelectDropdown from 'react-native-select-dropdown';
 
-const consumerTransactions = [
-    { value: 'All' },
-    { value: 'Incoming transactions' },
-    { value: 'Outgoing transactions' },
-    { value: 'Load ups B$' },
-    { value: 'Cash out to USD' }
-];
+const consumerTransactionTypes = ["All", "Incoming transactions", "Outgoing transactions", "Load ups B$", "Cash out to USD"];
 
 const styles = StyleSheet.create({
     container: {
@@ -51,24 +46,21 @@ const styles = StyleSheet.create({
 		color: colors.bodyText
 	},
     typeView: {
+        flex: 1,
         height: 55,
         justifyContent: 'center',
         marginTop: 7,
         backgroundColor: colors.inputBg
     },
-    pickerView: {
+    pickerText: {
         color: colors.darkGreen
     },
-    itemText: {
-        paddingLeft: 10
+    selectItem: {
+        width: '100%',
+        height: 55,
+        backgroundColor: colors.inputBg,
     },
-    dropdownContainer: {
-        paddingHorizontal: 20, 
-        paddingBottom: 20
-    },
-    dropdownInputContainer: {
-        borderBottomColor: 'transparent'
-    },
+    dropdownContainer: {marginTop: -20},
     placeholder: {
         color: colors.lightGreen
     },
@@ -115,7 +107,7 @@ const MyTransactionFilter = (): JSX.Element => {
                 <View style={styles.dateView}>
                     <Text style={styles.label}>{Translation.LABEL.START_DATE}</Text>
                     <TouchableOpacity onPress={()=>setIsStartDate(true)} style={styles.date} >
-                        <Text style={startDate == null ? styles.placeholder : styles.pickerView}>
+                        <Text style={startDate == null ? styles.placeholder : styles.pickerText}>
                             {startDate == null ? "MM/DD/YY" : moment(startDate).format('DD/MM/yyyy')}
                         </Text>
                     </TouchableOpacity>
@@ -124,7 +116,7 @@ const MyTransactionFilter = (): JSX.Element => {
                 <View style={styles.dateView}>
                     <Text style={styles.label}>{Translation.LABEL.END_DATE}</Text>
                     <TouchableOpacity onPress={()=>setIsEndDate(true)} style={styles.date}>
-                        <Text style={endDate == null ? styles.placeholder : styles.pickerView}>
+                        <Text style={endDate == null ? styles.placeholder : styles.pickerText}>
                             {endDate == null ? "MM/DD/YY" : moment(endDate).format('DD/MM/yyyy')}
                         </Text>
                     </TouchableOpacity>
@@ -132,17 +124,29 @@ const MyTransactionFilter = (): JSX.Element => {
             </View>
             <Text style={styles.label}>{Translation.LABEL.TRANSACTION_TYPE}</Text>
             <View style={styles.typeView}>
-                <Dropdown
-                    value={selectedType}
-                    data={consumerTransactions}
-                    selectedItemColor={colors.darkGreen}
-                    itemColor={colors.lightGreen}
-                    textColor={colors.darkGreen}
-                    itemTextStyle={styles.itemText}
-                    containerStyle={styles.dropdownContainer}
-                    inputContainerStyle={styles.dropdownInputContainer}
-                    onChangeText={(itemValue) => setSelectedType(itemValue)}
-                /> 
+                <SelectDropdown
+                    data={consumerTransactionTypes}
+                    defaultValueByIndex={0}
+                    onSelect={(selectedItem) => {
+                        setSelectedType(selectedItem)
+                    }}
+                    buttonTextAfterSelection={(selectedItem) => {
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item
+                    }}
+                    buttonStyle={styles.selectItem}
+                    buttonTextStyle={styles.pickerText}
+                    rowStyle={styles.selectItem}
+                    dropdownStyle={styles.dropdownContainer}
+                    renderCustomizedRowChild={(item) => (
+                        <Text style={styles.pickerText}>{item}</Text>
+                    )}
+                    renderDropdownIcon={() => (
+                        <AntDesign name="down" size={18} color={colors.darkGreen} />
+                    )}
+                />
             </View>
             <TouchableOpacity style={styles.clearFilter} onPress={clearFilter}>
                 <Text style={styles.clearText}>{Translation.PAYMENT.CLEAR_FILTER}</Text>
