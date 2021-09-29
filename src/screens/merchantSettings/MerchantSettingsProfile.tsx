fb85579a44c5f4b3f25b1e2e741c6b9eaaafa413
JeyDev310 <mtroz310@gmail.com>
2021-09-29 15:55:23 +0800

    feat: updated merchant profile page

--- src/screens/merchantSettings/MerchantSettingsProfile.tsx
+++ src/screens/merchantSettings/MerchantSettingsProfile.tsx
@@ -1,19 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState, useEffect, ReactElement } from 'react';
-import { ScrollView, StyleSheet, View, TouchableOpacity, Platform, Image, KeyboardAvoidingView} from "react-native";
-import { Text } from "react-native-elements";
+import React, { useContext, useState } from 'react';
+import { ScrollView, StyleSheet, View, TouchableOpacity, TextInput, Dimensions } from "react-native";
+import { Text, Image } from "react-native-elements";
 import * as ImagePicker from 'expo-image-picker';
-import { useUserDetails } from "src/hooks";
-import { Header, BackBtn, BlockInput, Button } from "src/shared/uielements";
+import { AuthContext } from "src/auth";
+import { IAuth } from "src/auth/types";
+import { useMediaLibraryPermission } from "src/hooks";
+import { Header, BackBtn, BlockInput, BusinessAddressForm } from "src/shared/uielements";
 import { colors } from "src/theme/colors";
-import { viewBase, underlineHeader } from "src/theme/elements";
+import { viewBaseB, underlineHeaderB } from "src/theme/elements";
 import Translation from 'src/translation/en.json';
-import { BUTTON_TYPES } from 'src/constants';
-
-interface ProfileState {
-	avatar: string;
-	username: string;
-}
 
 const styles = StyleSheet.create({
 	container: { 
@@ -23,79 +19,101 @@ const styles = StyleSheet.create({
 	headerText: {
 		fontSize: 32,
 		fontWeight: '400',
-		lineHeight: 40
+		lineHeight: 40,
+		color: colors.purple
 	},
 	contentView: {
 		marginTop: 50,
-		paddingBottom: 40
+		paddingBottom: 140
+	},
+	bannerImageView: {
+		position: 'relative',
+		borderRadius: 3,
+		backgroundColor: colors.white,
+		height: 220,
+		marginBottom: 60
+	},
+	bannerImage: {
+		width: '100%',
+		height: 220
+	},
+	pickImageView: {
+		position: 'absolute',
+		bottom: -65,
+		left: (Dimensions.get('window').width / 2) - 40,
+		paddingTop: 30,
+		paddingBottom: 30,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	label: {
+		color: colors.bodyText
+	},
+	smallLabel: {
+		color: colors.bodyText,
+		fontSize: 10
+	},
+	inputBg: {
+		color: colors.purple,
+		backgroundColor: colors.white
+	},
+	storyText: {
+		backgroundColor: colors.white,
+		borderRadius: 3,
+		paddingHorizontal: 15,
+		paddingVertical: 8,
+		marginVertical: 8,
+		borderWidth: 0,
+		color: colors.purple,
 	},
 	imageView: {
-		flex: 1,
-		alignSelf: 'center'
-	},
-	placeholder: {
 		width: 80, 
-		height: 80, 
-		backgroundColor: colors.darkGreen,
+		height: 80,
+		borderRadius: 40,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	image: {
+		width: 80,
+		height: 80,
 		borderRadius: 40
 	},
-	imageDesc1: {
-		fontSize: 16,
-		color: colors.bodyText, 
-		textAlign: "center", 
-		marginTop: 10,
+	scanButton: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '90%',
+		height: 55,
+		position: 'absolute',
+		bottom: 45,
+		color: colors.white,
+		backgroundColor: colors.purple,
+		alignSelf: 'center',
+		borderRadius: 30
 	},
-	imageDesc2: {
-		fontSize: 10,
-		textAlign: "center", 
-		marginBottom: 10
-	},
-	bottomView: {
-		paddingHorizontal: 20,
-		paddingBottom: 60,
+	scanBtnText: {
+		color: colors.white
 	}
 });
 
-export const MerchantSettingsDetails = (): ReactElement => {
+export const MerchantSettingsProfile = (): JSX.Element => {
 	const navigation = useNavigation();
-	const { personalDetails, updatePersonalDetails } = useUserDetails();
-	const [canSave, setCanSave] = useState<boolean>(false);
-	const [state, setState] = useState<ProfileState>({
-		avatar: "",
-		username: ""
-	});
 
-	useEffect(() => {
-		setCanSave(state.username !== "");
-	}, [state]);
+	const { buisnessBasicVerification, setBuisnessBasicVerification } =
+		useContext(AuthContext);
+	const [bannerImage, setBannerImage] = useState<string>("");
 
-	useEffect(() => {
-		setState({
-			avatar: personalDetails.avatar,
-			username: personalDetails.username
-		});
-	}, [personalDetails]);
-
-	useEffect(() => {
-		(async () => {
-		  if (Platform.OS !== 'web') {
-			const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-			if (status !== 'granted') {
-			  alert(Translation.OTHER.NO_CAMERA_PERMISSION);
-			}
-		  }
-		})();
-	}, []);
+	useMediaLibraryPermission();
 
 	const onValueChange = (name: string, change: string) => {
-		setState({
-		  ...state,
-		  [name]: change,
-		} as ProfileState);
-		updatePersonalDetails({ [name]: change });
+		setBuisnessBasicVerification((pv: IAuth) => ({ ...pv, [name]: change }));
 	};
 
-	const pickImage = async () => {
+	const handleSave = () => {
+		navigation.goBack();
+	}
+
+	const pickImage = async (name: string) => {
 		const result = await ImagePicker.launchImageLibraryAsync({
 			mediaTypes: ImagePicker.MediaTypeOptions.All,
 			allowsEditing: true,
@@ -104,52 +122,102 @@ export const MerchantSettingsDetails = (): ReactElement => {
 		});
 	
 		if (!result.cancelled) {
-		  	onValueChange('avatar', result.uri);
+			name === 'banner' ? setBannerImage(result.uri) : onValueChange('avatar', result.uri);
 		}
 	};
 
 	return (
-		<View style={viewBase}>
+		<View style={viewBaseB}>
 			<Header
-				leftComponent={<BackBtn onClick={() => navigation.goBack()} />}
+				leftComponent={<BackBtn color={colors.purple} onClick={() => navigation.goBack()} />}
 			/>
 			<ScrollView style={styles.container}>
-				<View style={ underlineHeader }>
+				<View style={ underlineHeaderB }>
 					<Text style={styles.headerText}>{Translation.PROFILE.MY_PROFILE}</Text>
 				</View>
-				<Text>{Translation.COMMUNITY_CHEST.INFORMATION_SHARE}</Text>
+				<Text style={styles.label}>{Translation.COMMUNITY_CHEST.INFORMATION_SHARE}</Text>
 				<View style={styles.contentView}>
-					<View style={styles.imageView}>
-						<TouchableOpacity onPress={pickImage}>
-						{state.avatar === '' && <View style={styles.placeholder} />}
-						{state.avatar !== '' && <Image source={{ uri: state.avatar }} style={styles.placeholder} />}
+					<View style={styles.bannerImageView}>
+						<TouchableOpacity onPress={()=>pickImage('banner')}>
+							{bannerImage === "" && (
+								<Image
+									source={require("../../../assets/images/profile-banner.png")}
+									containerStyle={styles.bannerImage}
+								/>
+							)}
+							{bannerImage !== "" && (
+								<Image
+									source={{ uri: bannerImage }}
+									style={styles.bannerImage}
+								/>
+							)}
 						</TouchableOpacity>
+
+						<View style={styles.pickImageView}>
+							<TouchableOpacity onPress={()=>pickImage('avatar')}>
+								{buisnessBasicVerification.avatar === "" && (
+									<View style={styles.imageView}>
+										<Image
+											source={require("../../../assets/images/placeholder4.png")}
+											containerStyle={styles.image}
+										/>
+									</View>
+								)}
+								{buisnessBasicVerification.avatar !== "" && (
+									<Image
+										source={{ uri: buisnessBasicVerification.avatar }}
+										style={styles.imageView}
+									/>
+								)}
+							</TouchableOpacity>
+						</View>
 					</View>
-					<Text style={styles.imageDesc1}>{Translation.PROFILE.CHANGE_PICTURE}</Text>
-					<Text style={styles.imageDesc2}>(MAX 200MB / JPG, JPEG, PNG)</Text>
-					<Text h3>{Translation.LABEL.USERNAME}</Text>
+					<Text style={styles.smallLabel}>
+						BUSINESS NAME - THIS NAME WILL BE PUBLIC*
+					</Text>
 					<BlockInput
-						name="username"
-						placeholder="@username"
-						value={state.username}
+						name="businessname"
+						placeholder="Business name"
+						placeholderTextColor={colors.greyedPurple}
+						value={buisnessBasicVerification.tag}
 						onChange={onValueChange}
+						style={styles.inputBg}
 					/>
+
+					<Text style={styles.smallLabel}>
+						TELL US YOUR STORY (50 WORDS MAX)
+					</Text>
+					<TextInput
+						placeholder="Tell the world about your business. What gives you joy as an entrepreneur? What do you love about the Berkshires?"
+						value={buisnessBasicVerification.story}
+						multiline={true}
+						onChangeText={(newValue) =>
+							onValueChange("story", newValue)
+						}
+						style={styles.storyText}
+						numberOfLines={4}
+					/>
+
+					<Text style={styles.smallLabel}>
+						WEBSITE - OPTIONAL
+					</Text>
+					<BlockInput
+						name="website"
+						placeholder="www.shop.com"
+						placeholderTextColor={colors.greyedPurple}
+						value={buisnessBasicVerification.website}
+						onChange={onValueChange}
+						style={styles.inputBg}
+					/>
+
+					<BusinessAddressForm  style={styles.inputBg} />
 				</View>
 			</ScrollView>
-			<KeyboardAvoidingView
-				behavior={Platform.OS == "ios" ? "padding" : "height"}
-			>
-				<View style={styles.bottomView}>
-					<Button
-						type={BUTTON_TYPES.PURPLE}
-						title={Translation.BUTTON.SAVE_CHANGE}
-						disabled={!canSave}
-						onPress={()=>setCanSave(true)}
-					/>
-				</View>
-			</KeyboardAvoidingView>
+			<TouchableOpacity onPress={handleSave} style={styles.scanButton}>
+				<Text style={styles.scanBtnText}>{Translation.BUTTON.SAVE_CHANGE}</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
 
-export default MerchantSettingsDetails;
+export default MerchantSettingsProfile;
