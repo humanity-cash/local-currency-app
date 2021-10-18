0dd5e8fbfc221a36a96c0e9e80ab6a19036020ef
JeyDev310 <mtroz310@gmail.com>
2021-10-19 02:31:29 +0800

    fix: loading page styling

--- src/screens/loadings/LoadingPage.tsx
+++ src/screens/loadings/LoadingPage.tsx
@@ -1,84 +1,16 @@
-import React, { useContext } from 'react';
-import { StyleSheet, View, ScrollView, ActivityIndicator } from 'react-native';
-import { Text } from 'react-native-elements';
-import { AuthContext } from 'src/auth';
-import { Modal, ModalHeader, Dialog } from "src/shared/uielements";
-import { modalViewBase, dialogViewBase } from "src/theme/elements";
-import { colors } from "src/theme/colors";
+import React from 'react';
 import { useLoadingModal } from "src/hooks";
 import { LoadingScreenTypes } from "src/utils/types";
-import { UserType } from 'src/auth/types';
-
-const styles = StyleSheet.create({
-    modalWrap: {
-		paddingHorizontal: 10,
-		marginBottom: 10
-	},
-	headerText: {
-		fontSize: 26,
-		lineHeight: 45,
-		marginBottom: 20,
-		paddingBottom: 10,
-	},
-    dialog: {
-        height: 300
-    },
-	dialogWrap: {
-		paddingHorizontal: 10,
-		height: "100%",
-		flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-	},
-    bodyText: {
-        fontSize: 16,
-        color: colors.bodyText,
-        lineHeight: 20
-    },
-    bottomView: {
-		paddingBottom: 45
-	}
-});
+import DataLoading from './DataLoading';
+import PaymentPending from './PaymentPending';
 
 const LoadingPage = (): JSX.Element => {
     const { details } = useLoadingModal();
-    const { userType } = useContext(AuthContext);
-
-    const getBackgroundColorStyle = () => {
-        return userType === UserType.Business ? {backgroundColor: colors.overlayPurple} : {backgroundColor: colors.lightGreen};
-    }
-
-    const getColorStyle = () => {
-        return userType === UserType.Business ? {color: colors.purple} : {color: colors.darkGreen};
-    }
 
     return details.screen === LoadingScreenTypes.PAYMENT_PENDING ? (
-        <Modal visible={details?.isLoading}>
-            <View style={ modalViewBase }>
-                <ModalHeader />
-                <ScrollView style={styles.modalWrap}>
-                    <Text style={styles.headerText}>Pending...</Text>
-                    <View>
-                        <Text style={styles.bodyText}>This usually takes 5-6 seconds</Text>
-                    </View>
-                </ScrollView>
-                <View style={styles.bottomView}>
-                    <ActivityIndicator size="large" color={userType === UserType.Business ? colors.purple : colors.darkGreen} />
-                </View>
-            </View>
-        </Modal>
+        <PaymentPending visible={details?.isLoading} />
     ) : (
-        <Dialog visible={details?.isLoading} style={styles.dialog} backgroundStyle={getBackgroundColorStyle} hiddenCloseBtn={true}>
-            <View style={dialogViewBase}>
-                <View style={styles.dialogWrap}>
-                    <Text style={{
-                        ...styles.headerText,
-                        ...getColorStyle
-                    }}>Loading...</Text>
-                    <ActivityIndicator size="large" color={userType === UserType.Business ? colors.purple : colors.darkGreen} />
-                </View>
-            </View>
-        </Dialog>
+        <DataLoading visible={details?.isLoading} />
     )
 }
 
