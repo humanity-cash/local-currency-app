5e5c72c3f83982dc7e682e16dd33a9f2ea75290c
JeyDev310 <mtroz310@gmail.com>
2021-10-20 17:17:09 +0800

    fix: removed auth initial values

--- src/auth/consts.ts
+++ src/auth/consts.ts
@@ -26,29 +26,29 @@ export const buisnessBasicVerificationInitialState: BusinessBasicVerification = 
 
 export const customerBasicVerificationInitialState: CustomerBasicVerification = {
 	type: "personal",
-	tag: "sat",
+	tag: "",
 	avatar: "",
-	firstName: "Satoshi",
-	lastName: "Nakamoto",
-	address1: "Satoshi Street 21",
-	address2: "Nakamoto Street 21",
-	city: "Sato",
-	state: "a",
-	postalCode: "2100000000",
+	firstName: "",
+	lastName: "",
+	address1: "",
+	address2: "",
+	city: "",
+	state: "",
+	postalCode: "",
 }
 
 export const dwollaInfoInitialState: DwollaInfo = {
-	dwollaId: "d9044dbb-ef9e-411a-a8a1-a710918ef569",
+	dwollaId: "",
 	resourceUri: "",
 }
 
 export const signInInitialState = {
-	email: "dev.s.ly813@gmail.com",
-	password: "Password123!@#",
+	email: "",
+	password: "",
 }
 
 export const signUpInitialState = {
-	email: "esraa@humanity.cash",
-	password: "Esraa@keyko1",
-	confirmPassword: "Esraa@keyko1",
+	email: "",
+	password: "",
+	confirmPassword: "",
 }
