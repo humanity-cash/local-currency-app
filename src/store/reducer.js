22083a06538012cc666e41c25f66fcbc3b44f28a
Esraa Jbara <jbesraa@gmail.com>
2021-11-26 22:58:42 +0200

    remove wallet reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 import { transactionReducer } from "./transaction/transaction.reducer";
-import { walletReducer } from "./wallet/wallet.reducer";
 import { loadingReducer } from "./loading/loading.reducer";
-import { fundingSourceReducer } from "./funding-source/funding-source.reducer";
 
 const reducer = combineReducers({
     transactionReducer,
-    walletReducer,
     loadingReducer,
-    fundingSourceReducer
 });
 
 export default reducer;
