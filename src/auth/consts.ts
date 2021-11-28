554d14da2f9bc866b7d7bb5dca1c34b5dc50d944
Esraa Jbara <jbesraa@gmail.com>
2021-11-28 23:28:43 +0200

    rm unused consts

--- src/auth/consts.ts
+++ src/auth/consts.ts
@@ -1,41 +1,4 @@
-import { BusinessBasicVerification, CustomerBasicVerification, DwollaInfo } from './types';
-
-export const buisnessBasicVerificationInitialState: BusinessBasicVerification = {
-	story: "",
-	tag: "",
-	avatar: "",
-	type: "",
-	owner: {
-		firstName: "",
-		lastName: "",
-		address1: "",
-		address2: "",
-		city: "",
-		state: "",
-		postalCode: "",
-	},
-	registeredBusinessName: "",
-	industry: "",
-	ein: "",
-	address1: "",
-	address2: "",
-	city: "",
-	state: "",
-	postalCode: "",
-}
-
-export const customerBasicVerificationInitialState: CustomerBasicVerification = {
-	type: "",
-	tag: "adedee",
-	avatar: "",
-	firstName: "dededea",
-	lastName: "deadeada",
-	address1: "deadeada",
-	address2: "deadada",
-	city: "dedee",
-	state: "CA",
-	postalCode: "2121",
-}
+import { DwollaInfo } from './types';
 
 export const dwollaInfoInitialState: DwollaInfo = {
 	dwollaId: "",
@@ -43,12 +6,12 @@ export const dwollaInfoInitialState: DwollaInfo = {
 }
 
 export const signInInitialState = {
-	email: "esraa+a2@humanity.cash",
-	password: "HumanityCash1122@",
+	email: "",
+	password: "",
 }
 
 export const signUpInitialState = {
-	email: "esraa+a2@humanity.cash",
-	password: "HumanityCash1122@",
-	confirmPassword: "HumanityCash1122@",
+	email: "",
+	password: "",
+	confirmPassword: "",
 }
