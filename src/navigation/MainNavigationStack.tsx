d417b97d275081dfff52fb806e9b840f003a35cb
Esraa Jbara <jbesraa@gmail.com>
2021-11-28 23:13:24 +0200

    fix navigation

--- src/navigation/MainNavigationStack.tsx
+++ src/navigation/MainNavigationStack.tsx
@@ -1,8 +1,11 @@
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { LoadingPageState } from 'src/store/loading/loading.reducer';
+import { AppState } from 'src/store';
+import { useSelector } from 'react-redux';
 import { createStackNavigator } from "@react-navigation/stack";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "react-native-gesture-handler";
-import { UserContext, AuthContext, WalletContext } from "src/contexts";
+import { UserContext, AuthContext, NavigationViewContext } from "src/contexts";
 import { AuthStatus, UserType } from "src/auth/types";
 import { useRouteTracking } from "src/hooks";
 import {
@@ -12,41 +15,18 @@ import {
   BusinessUserStack,
   NotVerifiedUserStack,
   LoadingStack,
+  CustomerLinkBankAccount,
+  BusinessLinkBankAccount
 } from "./stacks";
+import { ViewState } from "src/contexts/navigation";
 
 const PrimaryStack = createStackNavigator();
 
 const PrimaryStackScreen = () => {
-  const { authStatus } = useContext(AuthContext);
-  const { isLoading: isWalletLoading, walletData } = useContext(WalletContext);
-  const { userType, user, isLoading: isUserLoading } = useContext(UserContext);
-  const isVerifiedCustomer = user?.verifiedCustomer || false;
-  const isVerifiedBusiness = user?.verifiedBusiness || false;
-  const userNotSignedIn = authStatus !== AuthStatus.SignedIn;
-  const isLoading = authStatus === AuthStatus.Loading || isUserLoading ;
-  console.log("🚀 ~ file: MainNavigationStack.tsx ~ line 27 ~ PrimaryStackScreen ~ isUserLoading", isUserLoading)
-  console.log("🚀 ~ file: MainNavigationStack.tsx ~ line 27 ~ PrimaryStackScreen ~ isLoading", isLoading)
-    console.log("🚀 ~ file: MainNavigationStack.tsx ~ line 32 ~ PrimaryStackScreen ~ authStatus", authStatus)
-  const userIdInWallet =  walletData?.userId;
-  const customerUser =
-    authStatus === AuthStatus.SignedIn
-    && userType === UserType.Customer
-    && isVerifiedCustomer;
-  const businessUser =
-    authStatus === AuthStatus.SignedIn
-    && userType === UserType.Business
-    && isVerifiedBusiness;
-  const cashierUser =
-    authStatus === AuthStatus.SignedIn &&
-    userType === UserType.Cashier &&
-    isVerifiedBusiness;
-  const notVerifiedUser = !customerUser
-    && !cashierUser
-    && !businessUser
-    && authStatus === AuthStatus.SignedIn
-    && userType === UserType.NotVerified
-    //@ts-ignore
-    && user !== 'initial'
+  const { selectedView } = useContext(NavigationViewContext)
+  console.log("🚀 ~ file: MainNavigationStack.tsx ~ line 27 ~ PrimaryStackScreen ~ selectedView", selectedView)
+  const { user } = useContext(UserContext);
+  const { loadingState } = useSelector((state: AppState) => state.loadingReducer) as LoadingPageState;
 
   return (
     <PrimaryStack.Navigator
@@ -54,17 +34,21 @@ const PrimaryStackScreen = () => {
         headerShown: false
       })}
     >
-      {isLoading ? (<>{LoadingStack()}</>) : userNotSignedIn ? (
-        <>{OnboardingStack()}</>
-      ) : customerUser ? (
-        <>{CustomerUserStack({ isVerifiedBusiness })}</>
-      ) : cashierUser ? (
-        <>{CashierUserStack()}</>
-      ) : businessUser ? (
-        <>{BusinessUserStack({ isVerifiedCustomer })}</>
-      )
-        : notVerifiedUser ? (
+      {loadingState.isLoading ? (<>{LoadingStack()}</>)
+        : selectedView === ViewState.Customer ? (
+          <>{CustomerUserStack({ isVerifiedBusiness: user?.verifiedBusiness || false })}</>
+        ) : selectedView === ViewState.Cashier ? (
+          <>{CashierUserStack()}</>
+        ) : selectedView === ViewState.Business ? (
+          <>{BusinessUserStack({ isVerifiedCustomer: user?.verifiedCustomer || false })}</>
+        ) : selectedView === ViewState.CustomerLinkBank ? (
+          <>{CustomerLinkBankAccount()}</>
+        ) : selectedView === ViewState.BusinessLinkBank ? (
+          <>{BusinessLinkBankAccount()}</>
+        ) : selectedView === ViewState.NotVerified ? (
           <>{NotVerifiedUserStack()}</>
+        ) : selectedView === ViewState.Onboarding ? (
+          <>{OnboardingStack()}</>
         ) : (
           <>{LoadingStack()}</>
         )}
@@ -93,3 +77,27 @@ export const MainNavigationStack = (): JSX.Element => {
     </NavigationContainer>
   );
 };
+
+
+  // useEffect(() => {
+  //   const customerUser =
+  //     (authStatus === AuthStatus.SignedIn
+  //       && userType === UserType.Customer
+  //       && user?.verifiedCustomer) || false;
+  //   const businessUser =
+  //     (authStatus === AuthStatus.SignedIn
+  //       && userType === UserType.Business
+  //       && user?.verifiedBusiness) || false;
+  //   const cashierUser =
+  //     (authStatus === AuthStatus.SignedIn &&
+  //       userType === UserType.Cashier &&
+  //       user?.verifiedBusiness) || false;
+  //   const notVerifiedUser = !customerUser
+  //     && !cashierUser
+  //     && !businessUser
+  //     && authStatus === AuthStatus.SignedIn
+  //     && userType === UserType.NotVerified
+  //   setState({ customerUser, businessUser, cashierUser, notVerifiedUser })
+  // }, [
+  //   authStatus, userType, user
+  // ])
