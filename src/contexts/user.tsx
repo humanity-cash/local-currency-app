f3b08b45e9041e38a3d3e2cb594859f66cd6b31b
Esraa Jbara <jbesraa@gmail.com>
2021-11-28 23:12:05 +0200

    fix contexts

--- src/contexts/user.tsx
+++ src/contexts/user.tsx
@@ -1,90 +1,15 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Business, Customer, IDBUser } from "@humanity.cash/types";
-import { LoadingScreenTypes } from 'src/utils/types';
-import { updateLoadingStatus } from 'src/store/loading/loading.actions';
+import React, { useEffect, useState } from "react";
+import { IDBUser } from "@humanity.cash/types";
 import { saveUserTypeToStorage } from "../auth/localStorage";
-import { AuthContext } from "./auth";
-import { AxiosPromiseResponse } from "src/api/types";
-import { addBusinessVerification, addCustomerVerification, createUser, getUserByEmail } from "src/api/user";
-import { AuthStatus, UserType } from "../auth/types";
-import { useDispatch } from "react-redux";
-import { ObjectId } from "mongoose";
+import { UserType } from "../auth/types";
 
-export const UserContext = React.createContext<IState>({
-	user: {} as IDBUser,
-	isLoading: false,
-	userType: UserType.NotVerified,
-	businessDwollaId: "",
-	customerDwollaId: "",
-	//@ts-ignore
-	createCustomer: async (u: Customer) => ({}),
-	//@ts-ignore
-	createBusiness: async (u: Customer) => {
-		return ({});
-	},
-	getCustomerData: () => undefined,
-	updateUserType: () => undefined,
-	updateCustomerData: (u: Customer) => ({}),
-	getBusinessData: () => undefined,
-	updateBusinessData: (u: Business) => ({}),
-});
-
-const initialUserState = {
-	dbId: "" as unknown as ObjectId,
-	consent: false,
-	email: '',
-	verifiedCustomer: false,
-	verifiedBusiness: false,
-	customer: 
-	{
-		firstName: "",
-		lastName: "",
-		address1: "",
-		address2: "",
-		city: "",
-		state: "",
-		postalCode: "",
-		avatar: "",
-		tag: "",
-		dwollaId: "",
-		resourceUri: "",
-	},
-	business: {
-		story: "",
-		tag: "",
-		avatar: "",
-		type: "",
-		rbn: "",
-		industry: "",
-		ein: "",
-		address1: "",
-		address2: "",
-		city: "",
-		state: "",
-		postalCode: "",
-		phoneNumber: "",
-		dwollaId: "",
-		resourceUri: "",
-		owner: {
-			firstName: "",
-			lastName: "",
-			address1: "",
-			address2: "",
-			city: "",
-			state: "",
-			postalCode: "",
-		}
-	}
-}
+export const UserContext = React.createContext<IState>({} as IState);
 
 interface IState {
 	isLoading: boolean;
-	getBusinessData: () => Business | undefined,
 	updateBusinessData: (u: any) => void,
-	createCustomer: () => Promise<boolean>,
-	updateUserType: (i: UserType) => void,
-	createBusiness: () => Promise<boolean>,
-	getCustomerData: () => Customer | undefined,
+	updateUserType: (i: UserType, userEmail: string) => void,
+	updateUserData: (u: any) => void,
 	updateCustomerData: (u: any) => void,
 	user: IDBUser | undefined;
 	businessDwollaId: string;
@@ -93,138 +18,41 @@ interface IState {
 }
 
 export const UserProvider: React.FunctionComponent = ({ children }) => {
-	const { authStatus, userEmail: userEmailFromCognito } = useContext(AuthContext);
-	//@ts-ignore
-	const [user, setUser] = useState<IDBUser>('initial');
+	const [user, setUser] = useState<IDBUser>({} as IDBUser);
 	const [isLoading, setLoading] = useState<boolean>(false);
-	const dispatch = useDispatch();
-	//@ts-ignore
-	const [userType, setUserType] = useState<UserType>(null);
-	const verifiedCustomer = user?.verifiedCustomer;
-	const verifiedBusiness = user?.verifiedBusiness;
+	const [userType, setUserType] = useState<UserType>("" as UserType);
 	const [customerDwollaId, setCustomerDwollaId] = useState("");
 	const [businessDwollaId, setBusinessDwollaId] = useState("");
-	const userEmail = user?.email?.length > 0 ? user.email : userEmailFromCognito;
+
+	const updateUserData = (uData: any) => setUser({ ...uData });
 	const updateCustomerData = (u: any) => {
 		setUser((pv: IDBUser) => ({ ...pv, customer: { ...pv.customer, ...u } }))
 	}
 	const updateBusinessData = (u: any) => {
 		setUser((pv: IDBUser) => ({ ...pv, business: { ...pv.business, ...u } }))
 	}
-	const getCustomerData = (): Customer | undefined => user?.customer;
-	const getBusinessData = (): Business | undefined => user?.business;
-	const updateUserType = (newType: UserType, uEmail = userEmail): void => {
+	const updateUserType = (newType: UserType, userEmail: string): void => {
 		setUserType(newType);
-		saveUserTypeToStorage(uEmail, newType);
+		saveUserTypeToStorage(userEmail, newType);
 	};
 
 	useEffect(() => {
-		const updateUserData = async () => {
-			setLoading(true);
-			if ((authStatus === AuthStatus.SignedIn) && userEmail) {
-				const userData = await getUserByEmail(userEmail);
-				if (userData) {
-					setUser({ ...userData })
-					return;
-				} else {
-					setUser(initialUserState)
-				}
-			}
-			setLoading(false);
-		}
-		updateUserData();
-	}, [authStatus, userEmail])
-
-	useEffect(() => {
 		setLoading(true);
-		if (userEmail && authStatus === AuthStatus.SignedIn) {
-			if (verifiedBusiness || verifiedCustomer) {
-				if (verifiedBusiness && verifiedCustomer) {
-					const latestSelectedType = UserType.Customer; // :FIXME
-					updateUserType(latestSelectedType)
-				} else if (verifiedCustomer) {
-					updateUserType(UserType.Customer)
-				} else if (verifiedBusiness) {
-					updateUserType(UserType.Business)
-				}
-				//@ts-ignore
-			} else if(user != "initial") {
-				updateUserType(UserType.NotVerified);
-			}
-		}
+		setBusinessDwollaId(user?.business?.dwollaId || "")
+		setCustomerDwollaId(user?.customer?.dwollaId || "")
 		setLoading(false);
-	}, [authStatus, user, userEmail])
-
-	useEffect(() => {
-		setLoading(true);
-		if (verifiedBusiness) {
-			setBusinessDwollaId(user?.business?.dwollaId || "")
-		}
-		if (verifiedCustomer) {
-			setCustomerDwollaId(user?.customer?.dwollaId || "")
-		}
-		setLoading(false);
-	}, [verifiedCustomer, verifiedBusiness, user])
-
-	const createBusiness = async (): Promise<boolean> => {
-		dispatch(updateLoadingStatus({
-			isLoading: true,
-			screen: LoadingScreenTypes.ANY
-		}));
-		if(!user.email) {
-			user.email = userEmail;
-		}
-		const response = await createBus(user);
-		if (isSuccessResponse(response)) {
-			const newUser: IDBUser = response?.data
-			if (newUser?.verifiedBusiness) {
-				setUser(newUser);
-			}
-		}
-		dispatch(updateLoadingStatus({
-			isLoading: false,
-			screen: LoadingScreenTypes.ANY
-		}));
-		return isSuccessResponse(response);
-	}
-
-	const createCustomer = async (): Promise<boolean> => {
-		dispatch(updateLoadingStatus({
-			isLoading: true,
-			screen: LoadingScreenTypes.ANY
-		}));
-		if (!user?.customer) return false
-		if(!user.email) {
-			user.email = userEmailFromCognito;
-		}
-		user.customer.avatar = "avatarlink";
-		const response = await createCus(user);
-		if (isSuccessResponse(response)) {
-			const newUser: IDBUser = response?.data
-			if (newUser?.verifiedCustomer) {
-				setUser(newUser);
-			}
-		}
-		dispatch(updateLoadingStatus({
-			isLoading: false,
-			screen: LoadingScreenTypes.ANY
-		}));
-		return isSuccessResponse(response);
-	}
+	}, [user])
 
 	const state: IState = {
 		user,
 		isLoading,
 		userType,
 		updateUserType,
-		getCustomerData,
 		updateCustomerData,
-		getBusinessData,
 		updateBusinessData,
-		createCustomer,
-		createBusiness,
 		businessDwollaId,
-		customerDwollaId
+		customerDwollaId,
+		updateUserData,
 	};
 
 	return (
@@ -232,57 +60,124 @@ export const UserProvider: React.FunctionComponent = ({ children }) => {
 	);
 };
 
-const isSuccessResponse = (response: AxiosPromiseResponse) => {
-	if (response.status >= 200 && response.status <= 299) {
-		return true
-	} else return false
-}
+	// useEffect(() => {
+	// 	updateUserData();
+	// }, [authStatus, userEmail])
 
-const createBus = async (user: IDBUser): Promise<AxiosPromiseResponse<IDBUser>> => {
-	try {
-		const isRegisteredUser = user?.verifiedCustomer
-			&& user?.customer?.dwollaId
-			&& user?.business;
-		if (isRegisteredUser) {
-			const response: AxiosPromiseResponse<IDBUser> = await addBusinessVerification(
-				//@ts-ignore
-				user.customer.dwollaId
-				, { ...user?.business, avatar: "avatar" });
-			return response;
-		} else {
-			const response = await createUser({
-				email: user.email,
-				consent: true,
-				type: 'business',
-				business: user.business
-			});
-			return response
-		}
-	} catch (error) {
-		console.log("error creating business", error)
-		return {} as AxiosPromiseResponse<IDBUser>;
-	}
-}
 
-const createCus = async (user: IDBUser): Promise<AxiosPromiseResponse<IDBUser>> => {
-	try {
-		const isRegisteredUser = user?.verifiedBusiness && user?.business?.dwollaId && user?.dbId && user?.customer;
-		if (isRegisteredUser) {
-			const response: AxiosPromiseResponse<IDBUser> =
-			//@ts-ignore
-				await addCustomerVerification(user.business.dwollaId, user?.customer);
-			return response;
-		} else {
-			const response = await createUser({
-				email: user.email,
-				consent: true,
-				type: 'customer',
-				customer: user.customer
-			});
-			return response
-		}
-	} catch (error) {
-		console.log("error creating business", error)
-		return {} as AxiosPromiseResponse<IDBUser>;
-	}
-}
+	// const typeUpdate = async () => {
+	// 	setLoading(true);
+	// 	if (userEmail && authStatus === AuthStatus.SignedIn) {
+	// 		const verifiedCustomer = user?.verifiedCustomer;
+	// 		const verifiedBusiness = user?.verifiedBusiness;
+	// 		if (verifiedBusiness || verifiedCustomer) {
+	// 			if (verifiedBusiness && verifiedCustomer) {
+	// 				const latestSelectedType = await getLatestSelectedAccountType(userEmail);
+	// 				updateUserType(latestSelectedType as UserType);
+	// 			} else if (verifiedCustomer) {
+	// 				updateUserType(UserType.Customer)
+	// 			} else if (verifiedBusiness) {
+	// 				updateUserType(UserType.Business)
+	// 			}
+	// 		} else {
+	// 			updateUserType(UserType.NotVerified);
+	// 		}
+	// 	}
+	// 	setLoading(false);
+	// }
+
+	// const updateUserData = async () => {
+	// 	setLoading(true);
+	// 	if ((authStatus === AuthStatus.SignedIn) && userEmail) {
+	// 		const userData = await UserAPI.getUserByEmail(userEmail);
+	// 		if (userData) {
+	// 			setUser({ ...userData })
+	// 			return;
+	// 		} else {
+	// 			setUser(initialUserState)
+	// 		}
+	// 	}
+	// 	setLoading(false);
+	// }
+
+// const initialUserState = {
+// 	dbId: "" as unknown as ObjectId,
+// 	consent: false,
+// 	email: '',
+// 	verifiedCustomer: false,
+// 	verifiedBusiness: false,
+// 	customer: 
+// 	{
+// 		firstName: "aaa",
+// 		lastName: "eee",
+// 		address1: "eeee",
+// 		address2: "eeee",
+// 		city: "eeee",
+// 		dwollaId: "",
+// 		state: "",
+// 		postalCode: "1232",
+// 		avatar: "",
+// 		tag: "dsadsa",
+// 	},
+// 	business: {
+// 		story: "ewqeqwew",
+// 		tag: "ewqeqw",
+// 		avatar: "eqwewq",
+// 		type: "eqwewq",
+// 		rbn: "eqwewqe",
+// 		industry: "ewqewq",
+// 		ein: "ewqewq",
+// 		address1: "eqwewq",
+// 		address2: "eqweqw",
+// 		city: "eqweqw",
+// 		state: "ewqeqw",
+// 		postalCode: "eqweqw",
+// 		phoneNumber: "eqweqw",
+// 		dwollaId: "",
+// 		resourceUri: "",
+// 		owner: {
+// 			firstName: "eqweqw",
+// 			lastName: "ewqeqw",
+// 			address1: "ewqewq",
+// 			address2: "eqwewq",
+// 			city: "ewqeqw",
+// 			state: "eqwewq",
+// 			postalCode: "eqweqw",
+// 		}
+// 	}
+// }
+
+// const createBusiness = async (): Promise<boolean> => {
+// if (!user.email) {
+// 	user.email = userEmail;
+// }
+// const response = await UserAPI.createBusiness(user);
+// if (isSuccessResponse(response)) {
+// 	const newUser: IDBUser = response?.data
+// 	if (newUser?.verifiedBusiness) {
+// 		setUser(newUser);
+// 		updateUserType(UserType.Business);
+// 	}
+// }
+// setLoading(false);
+// return isSuccessResponse(response);
+// }
+
+// const createCustomer = async (): Promise<boolean> => {
+// setLoading(true);
+// if (!user?.customer) return false
+// if (!user.email) {
+// 	user.email = userEmail;
+// }
+// user.customer.avatar = "avatarlink";
+// const response = await UserAPI.createCustomer(user);
+// if (isSuccessResponse(response)) {
+// 	const newUser: IDBUser = response?.data
+// 	if (newUser?.verifiedCustomer) {
+// 		setUser(newUser);
+// 		updateUserType(UserType.Customer);
+// 	}
+// }
+// setLoading(false);
+// return isSuccessResponse(response);
+// }
