511cc531a0e7c02fc9daaf0366099d2e4af92b0a
Esraa Jbara <jbesraa@gmail.com>
2021-12-14 15:55:27 +0200

    rm not used

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import { transactionReducer } from "./transaction/transaction.reducer";
 import { loadingReducer } from "./loading/loading.reducer";
 
 const reducer = combineReducers({
-    transactionReducer,
     loadingReducer,
 });
 
