e40acca56a39e0bf39fb9924b1f33862645e300d
Esraa Jbara <jbesraa@gmail.com>
2021-12-15 13:39:01 +0200

    safe request

--- src/api/dwolla.ts
+++ src/api/dwolla.ts
@@ -6,6 +6,7 @@ import { getUser } from './user';
 
 export const iavToken = async (userId: UserId): Promise<AxiosPromiseResponse> => {
   try {
+    if(!userId) return {} as AxiosPromiseResponse;
     const response = await postRequest(`/users/${userId}/iav-token`, {});
     return response;
   } catch (err) {
@@ -19,7 +20,7 @@ export const loadFundingSource = async (userId: UserId): Promise<boolean> => {
     const response = await getRequest(`/users/${userId}/funding-sources`);
     return fundingSource(response);
   } catch (err) {
-    console.log(err);
+    console.log('error in foundingsource', err);
     return false;
   }
 };
