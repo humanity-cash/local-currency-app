4910b7ef9debcf7fa0d922e6076f409a1aac1269
JeyDev310 <mtroz310@gmail.com>
2021-12-15 16:59:21 +0800

    update terms text

--- src/screens/settings/SettingsTermsAndConditions.tsx
+++ src/screens/settings/SettingsTermsAndConditions.tsx
@@ -26,8 +26,13 @@ const styles = StyleSheet.create({
 const termsAndConditions: AccordionEntry = {
   title: "Terms & Conditions",
   content: "ARTICLE 1: GENERAL \n\nSection 1: Name -  The name of the corporation will be BerkShares, Inc., a non-profit corporation organized under the laws of the Commonwealth of Massachusetts.\n" +
-		"Section 2: Area - The corporation will focus its activities in the Berkshire Region.\n" + 
-		"Section 3: Offices - The principal office of the corporation will be at 140 Jug End Road, South Egremont, Massachusetts 01258 (mailing address: P O Box 125, Great Barrington, MA 01230) or at some other physical location as determined by the Board of Trustees."
+		"Section 2: Area - The corporation will focus its activities in the Berkshire Region.\n" +
+		"Section 3: Offices - The principal office of the corporation will be at 140 Jug End Road, South Egremont, Massachusetts 01258 (mailing address: P O Box 125, Great Barrington, MA 01230) or at some other physical location as determined by the Board of Trustees.\n\n" +
+		"ARTICLE II: Purpose\n\n" +  
+		"Section 1: Purpose - The Purpose of BerkShares, Inc. is to initiate, encourage and administer educational and practical programs for the furtherance of regional economic self-reliance in the Berkshire Region.\n\n" +
+		"By pooling capital and human resources, BerkShares, Inc. seeks to facilitate the formation of small businesses, cottage industries, farms and cooperatives that would enable local communities to develop greater self-reliance. Basic human needs in the areas of food, shelter, energy, environment, employment, transportation, health care, education, cultural activities and social services could thus be increasingly met through local efforts. It is intended that such a program would encourage use of land in harmony with ecological principles. It would also encourage the development of alternative exchange instruments and of community associations that would foster and support initiative in these areas.\n\n" +
+		"ARTICLE III: Membership\n\n" +
+		"Section 1: General Membership - The membership of BerkShares, Inc. will be open to all residents of the Berkshire Region who are interested in the promotion of local and regional economic self-sufficiency. All Members will be considered in good standing if they have paid an annual membership fee as established by the Board of Trustees.\n"
 }
 
 const privacyPolicy: AccordionEntry = {
