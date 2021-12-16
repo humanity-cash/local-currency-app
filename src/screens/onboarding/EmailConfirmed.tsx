2670a3a478ce9744e3e92be436eec113203d725a
Esraa Jbara <jbesraa@gmail.com>
2021-12-16 19:16:19 +0200

    fix-user-notconfirmed-flow

--- src/screens/onboarding/EmailConfirmed.tsx
+++ src/screens/onboarding/EmailConfirmed.tsx
@@ -10,71 +10,73 @@ import { baseHeader, viewBase, wrappingContainerBase } from "src/theme/elements"
 import DataLoading from 'src/screens/loadings/DataLoading';
 
 const styles = StyleSheet.create({
-	headerText: {
-		fontSize: 32,
-		color: colors.darkGreen,
-		lineHeight: 35
-	},
-	bodyText: {
-		color: colors.bodyText
-	},
-	bottomNavigation: {
-		justifyContent: "center"
-	},
-	bottomView: {
-		marginHorizontal: 20,
+    headerText: {
+        fontSize: 32,
+        color: colors.darkGreen,
+        lineHeight: 35
+    },
+    bodyText: {
+        color: colors.bodyText
+    },
+    bottomNavigation: {
+        justifyContent: "center"
+    },
+    bottomView: {
+        marginHorizontal: 20,
         marginBottom: 20
-	},
-	image: {
-		alignSelf: "center",
-		width: 280,
-		height: 280
-	},
-	imageView: {
-		justifyContent: "center",
-		textAlignVertical: "center",
-		flex: 1
-	}
+    },
+    image: {
+        alignSelf: "center",
+        width: 280,
+        height: 280
+    },
+    imageView: {
+        justifyContent: "center",
+        textAlignVertical: "center",
+        flex: 1
+    }
 });
 
 const EmailConfirmed = (): JSX.Element => {
-	const navigation = useNavigation();
-	const { signUpDetails: { email, password }, signIn } = useContext(AuthContext);
-	const [isLoading, setLoading] = useState<boolean>(false)
+    const navigation = useNavigation();
+    const { signUpDetails, signIn, signInDetails } = useContext(AuthContext);
+    const [isLoading, setLoading] = useState<boolean>(false)
+    const email = signUpDetails?.email || signInDetails?.email;
+    const password = signUpDetails?.password || signInDetails?.password;
 
-	const onNext = async () => {
-		setLoading(true)
-		await signIn(email, password)
-		setLoading(false)
-	}
+    const onNext = async () => {
+        setLoading(true)
+        await signIn(email, password)
+        setLoading(false)
+    }
 
-	return (
-		<View style={viewBase}>
-			<DataLoading visible={isLoading} />
-			<Header
-				leftComponent={
-					<BackBtn onClick={() => navigation.goBack()} />
-				}
-			/>
-			<View style={wrappingContainerBase}>
-				<View style={baseHeader}>
-					<Text style={styles.headerText}>
-						Mail address confirmed!
-					</Text>
-				</View>
-				<Text style={styles.bodyText}>
-					Your email address {email} is confirmed.
-				</Text>
-			</View>
-			<SafeAreaView style={styles.bottomView}>
-				<Button
-					type={BUTTON_TYPES.DARK_GREEN}
-					title='NEXT'
-					onPress={onNext}
-				/>
-			</SafeAreaView>
-		</View>
-	);
+    return (
+        <View style={viewBase}>
+            <DataLoading visible={isLoading} />
+            <Header
+                leftComponent={
+                    <BackBtn onClick={() => navigation.goBack()} />
+                }
+            />
+            <View style={wrappingContainerBase}>
+                <View style={baseHeader}>
+                    <Text style={styles.headerText}>
+                        Mail address confirmed!
+                    </Text>
+                </View>
+                <Text style={styles.bodyText}>
+                    Your email address {email} is confirmed.
+                </Text>
+            </View>
+            <SafeAreaView style={styles.bottomView}>
+                <Button
+                    type={BUTTON_TYPES.DARK_GREEN}
+                    title='NEXT'
+                    onPress={onNext}
+                />
+            </SafeAreaView>
+        </View>
+    );
 }
 
 export default EmailConfirmed;
