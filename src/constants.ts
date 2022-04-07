91e6a67ab0655b3c4fcb03f864952f968964cee6
JeyDev310 <mtroz310@gmail.com>
2022-04-07 06:46:36 -0400

    profile size from constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,11 @@ export const BUTTON_TYPES = {
   PURPLE: "purple",
 };
 
+export const DEFAULT_PROFILE_WIDTH          = 128
+export const DEFAULT_PROFILE_HEIGHT         = 128
+export const DEFAULT_PROFILE_BANNER_WIDTH   = 512
+export const DEFAULT_PROFILE_BANNER_HEIGHT  = 512
+
 /**AWS COGNITO ERRORS */
 export const COGNITO_USERNAME_EXISTS_EXCEPTION_CODE = "UsernameExistsException";
 export const NEW_PASSWORD_REQUIRED_ERROR = "newPasswordRequiredError";
